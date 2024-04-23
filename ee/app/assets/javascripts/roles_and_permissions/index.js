e5767f0f2159746925d68729c5d2bd8155e23857
Hinam Mehra <hmehra@gitlab.com>
2024-04-23 11:29:29 +1000

    Remove legacy custom roles UI
    - With the new custom roles UI now rolled out,
    we can delete the legacy custom roles UI

--- ee/app/assets/javascripts/roles_and_permissions/index.js
+++ ee/app/assets/javascripts/roles_and_permissions/index.js
@@ -2,7 +2,6 @@ import { GlToast } from '@gitlab/ui';
 import Vue from 'vue';
 import VueApollo from 'vue-apollo';
 import createDefaultClient from '~/lib/graphql';
-import ListMemberRoles from './components/list_member_roles.vue';
 import CustomRolesApp from './components/app.vue';
 
 Vue.use(GlToast);
@@ -11,27 +10,6 @@ Vue.use(VueApollo);
 const apolloProvider = new VueApollo({
   defaultClient: createDefaultClient(),
 });
-
-export const initRolesAndPermissions = () => {
-  const el = document.querySelector('#js-roles-and-permissions');
-
-  if (!el) {
-    return null;
-  }
-
-  const { groupFullPath } = el.dataset;
-
-  return new Vue({
-    el,
-    name: 'RolesAndPermissionsRoot',
-    apolloProvider,
-    render(createElement) {
-      return createElement(ListMemberRoles, {
-        props: { groupFullPath },
-      });
-    },
-  });
-};
 
 export const initCustomRolesApp = () => {
   const el = document.querySelector('#js-roles-and-permissions');
