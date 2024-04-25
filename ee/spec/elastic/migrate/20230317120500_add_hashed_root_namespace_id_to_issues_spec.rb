d7a51a6b06134b47d8c1959d77172cc68b1c8baf
Terri Chu <tchu@gitlab.com>
2024-04-25 11:20:40 -0400

    Mark AddHashedRootNamespaceIdToIssues as obsolete
    This migration marks the 20230317120500 AddHashedRootNamespaceIdToIssues
    Advanced search migration as obsolete.
    
    This MR will still need changes to remove references to the migration in the code.
    At the moment the `gitlab-housekeeper` is not always capable of removing all references so you must check the
    diff and pipeline failures to confirm if there are any issues.
    It is the responsibility of the assignee (picked from ~"group::global search") to push those changes to this branch.
    
    You can read more about the process for marking Advanced search migrations as obsolete in
    https://docs.gitlab.com/ee/development/search/advanced_search_migration_styleguide.html#deleting-advanced-search-migrations-in-a-major-version-upgrade.
    
    As part of our process we want to ensure all Advanced search migrations have had at least one
    [required stop](https://docs.gitlab.com/ee/development/database/required_stops.html)
    to process the migration. Therefore we can mark any Advanced search migrations added before the
    last required stop as obsolete.
    
    This change was generated by
    [gitlab-housekeeper](https://gitlab.com/gitlab-org/gitlab/-/tree/master/gems/gitlab-housekeeper)
    using the Keeps::MarkOldAdvancedSearchMigrationsAsObsolete keep.
    
    To provide feedback on your experience with `gitlab-housekeeper` please comment in
    <https://gitlab.com/gitlab-org/gitlab/-/issues/442003>.
    
    Changelog: other
    EE: true

--- ee/spec/elastic/migrate/20230317120500_add_hashed_root_namespace_id_to_issues_spec.rb
+++ ee/spec/elastic/migrate/20230317120500_add_hashed_root_namespace_id_to_issues_spec.rb
@@ -5,7 +5,5 @@ require_relative 'migration_shared_examples'
 require File.expand_path('ee/elastic/migrate/20230317120500_add_hashed_root_namespace_id_to_issues.rb')
 
 RSpec.describe AddHashedRootNamespaceIdToIssues, :elastic, :sidekiq_inline, feature_category: :global_search do
-  let(:version) { 20230317120500 }
-
-  include_examples 'migration adds mapping'
+  it_behaves_like 'a deprecated Advanced Search migration', 20230317120500
 end
