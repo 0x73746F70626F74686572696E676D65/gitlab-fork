2af7e14e5c9642adce1b0464155cfc906d32b26a
Terri Chu <tchu@gitlab.com>
2024-04-25 11:19:31 -0400

    Mark 20230316150000 search migration as obsolete
    This migration marks the 20230316150000 AddHashedRootNamespaceIdToMergeRequests Advanced search migration as obsolete.
    
    This MR will still need changes to remove references to the migration in the code.
    At the moment the `gitlab-housekeeper` is not always capable of removing all references so you must check the
    diff and pipeline failures to confirm if there are any issues.
    It is the responsibility of the assignee (picked from ~"group::global search") to push those changes to this branch.
    
    You can read more about the process for marking Advanced search migrations as obsolete in
    https://docs.gitlab.com/ee/development/search/advanced_search_migration_styleguide.html#deleting-advanced-search-migrations-in-a-major-version-upgrade.
    
    As part of our process we want to ensure all Advanced search migrations have had at least one
    [required stop](https://docs.gitlab.com/ee/development/database/required_stops.html)
    to process the migration. Therefore we can mark any Advanced search migrations added before the
    last required stop as obsolete.
    
    This change was generated by
    [gitlab-housekeeper](https://gitlab.com/gitlab-org/gitlab/-/tree/master/gems/gitlab-housekeeper)
    using the Keeps::MarkOldAdvancedSearchMigrationsAsObsolete keep.
    
    To provide feedback on your experience with `gitlab-housekeeper` please comment in
    <https://gitlab.com/gitlab-org/gitlab/-/issues/442003>.
    
    Changelog: other
    EE: true

--- ee/spec/elastic/migrate/20230316150000_add_hashed_root_namespace_id_to_merge_requests_spec.rb
+++ ee/spec/elastic/migrate/20230316150000_add_hashed_root_namespace_id_to_merge_requests_spec.rb
@@ -5,7 +5,5 @@ require_relative 'migration_shared_examples'
 require File.expand_path('ee/elastic/migrate/20230316150000_add_hashed_root_namespace_id_to_merge_requests.rb')
 
 RSpec.describe AddHashedRootNamespaceIdToMergeRequests, :elastic, :sidekiq_inline, feature_category: :global_search do
-  let(:version) { 20230316150000 }
-
-  include_examples 'migration adds mapping'
+  it_behaves_like 'a deprecated Advanced Search migration', 20230316150000
 end
