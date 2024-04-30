c206cfc04b7e5334e0d6de58416aa86dcd1b46df
Miguel Rincon <mrincon@gitlab.com>
2024-04-30 15:21:51 +0200

    Removes "new" badge next to fleet dashboard link
    Changelog: changed

--- ee/spec/frontend/ci/runner/components/runner_dashboard_link_spec.js
+++ ee/spec/frontend/ci/runner/components/runner_dashboard_link_spec.js
@@ -1,4 +1,4 @@
-import { GlBadge, GlButton } from '@gitlab/ui';
+import { GlButton } from '@gitlab/ui';
 import { s__ } from '~/locale';
 import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
 import RunnerDashboardLink from 'ee_component/ci/runner/components/runner_dashboard_link.vue';
@@ -8,7 +8,6 @@ import { runnerDashboardPath } from 'ee_jest/ci/runner/mock_data';
 describe('RunnerDashboardLink', () => {
   let wrapper;
 
-  const findBadge = () => wrapper.findComponent(GlBadge);
   const findButton = () => wrapper.findComponent(GlButton);
 
   const createComponent = (options) => {
@@ -30,11 +29,6 @@ describe('RunnerDashboardLink', () => {
       expect(findButton().text()).toContain(s__('Runners|Fleet dashboard'));
       expect(findButton().props('variant')).toBe('link');
       expect(findButton().attributes('href')).toBe(runnerDashboardPath);
-    });
-
-    it('renders badge', () => {
-      expect(findBadge().props()).toMatchObject({ variant: 'info', size: 'sm' });
-      expect(findBadge().text()).toContain(s__('Runners|New'));
     });
   });
 
