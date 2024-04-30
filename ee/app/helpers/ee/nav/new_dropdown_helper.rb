97cf1da14153d3efa8e26012a5bd1b251d443e95
Jack Chapman <jachapman@gitlab.com>
2024-04-30 14:10:47 +0000

    Add modal for epic work item creation
    When the namespace_level_work_items feature is
    enabled, the create -> new epic link will open
    a modal instead of navigating to a different page
    
    Changelog: changed
    EE: true

--- ee/app/helpers/ee/nav/new_dropdown_helper.rb
+++ ee/app/helpers/ee/nav/new_dropdown_helper.rb
@@ -10,16 +10,29 @@ module EE
       override :create_epic_menu_item
       def create_epic_menu_item(group)
         if can?(current_user, :create_epic, group)
-          ::Gitlab::Nav::TopNavMenuItem.build(
-            id: 'create_epic',
-            title: _('New epic'),
-            href: new_group_epic_path(group),
-            data: {
-              track_action: 'click_link_new_epic',
-              track_label: 'plus_menu_dropdown',
-              track_property: 'navigation_top'
-            }
-          )
+          if ::Feature.enabled?(:namespace_level_work_items, group)
+            ::Gitlab::Nav::TopNavMenuItem.build(
+              id: 'create_epic',
+              title: _('New epic'),
+              component: 'create_new_work_item_modal',
+              data: {
+                track_action: 'click_link_new_epic',
+                track_label: 'plus_menu_dropdown',
+                track_property: 'navigation_top'
+              }
+            )
+          else
+            ::Gitlab::Nav::TopNavMenuItem.build(
+              id: 'create_epic',
+              title: _('New epic'),
+              href: new_group_epic_path(group),
+              data: {
+                track_action: 'click_link_new_epic',
+                track_label: 'plus_menu_dropdown',
+                track_property: 'navigation_top'
+              }
+            )
+          end
         end
       end
     end
