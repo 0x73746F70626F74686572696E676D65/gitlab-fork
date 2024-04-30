2f628d6d9d0e0a5d48ab570d5baf58d601c9dd33
Serena Fang <sfang@gitlab.com>
2024-04-30 19:37:56 +0000

    Fix project audit events
    Changelog: added
    
    Regenerate audit event docs
    
    Remove hardcoded value
    
    Fix current user
    
    Fix secret detection specs
    
    Change yml to more general project security setting changed
    
    Update audit event docs
    
    Use super to inherit

--- ee/app/services/security/configuration/set_pre_receive_secret_detection.rb
+++ ee/app/services/security/configuration/set_pre_receive_secret_detection.rb
@@ -3,17 +3,24 @@
 module Security
   module Configuration
     class SetPreReceiveSecretDetection
-      def self.execute(namespace:, enable:)
+      def self.execute(current_user:, namespace:, enable:)
         # At present, the security_setting feature is exclusively accessible for projects.
         # Following the implementation of https://gitlab.com/gitlab-org/gitlab/-/issues/451357,
         # this feature will also be available at the group level.
-        ServiceResponse.success(
+        response = ServiceResponse.success(
           payload: {
             enabled: namespace.security_setting.set_pre_receive_secret_detection!(
               enabled: enable
             ),
             errors: []
           })
+
+        if response.success?
+          Audit::ProjectSecuritySettingChangesAuditor.new(
+            current_user: current_user, model: namespace.security_setting).execute
+        end
+
+        response
       rescue StandardError => e
         ServiceResponse.error(
           message: e.message,
