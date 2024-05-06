44074a084bd5eea4c8e019a0f058ed59c191f711
Artur Fedorov <afedorov@gitlab.com>
2024-05-06 11:50:34 +0000

    This MR adds integration test
    New integration tests are required
    for new pipeline execution policy

--- ee/spec/frontend_integration/security_orchestration/policies_list/app_spec.js
+++ ee/spec/frontend_integration/security_orchestration/policies_list/app_spec.js
@@ -7,9 +7,12 @@ import {
   groupScanExecutionPolicies,
   projectScanResultPolicies,
   groupScanResultPolicies,
+  projectPipelineResultPolicies,
+  groupPipelineResultPolicies,
 } from 'ee_jest/security_orchestration/mocks/mock_apollo';
 import { mockScanExecutionPoliciesResponse } from 'ee_jest/security_orchestration/mocks/mock_scan_execution_policy_data';
 import { mockScanResultPoliciesResponse } from 'ee_jest/security_orchestration/mocks/mock_scan_result_policy_data';
+import { mockPipelineExecutionPoliciesResponse } from 'ee_jest/security_orchestration/mocks/mock_pipeline_execution_policy_data';
 import ListHeader from 'ee/security_orchestration/components/policies/list_header.vue';
 import ListComponent from 'ee/security_orchestration/components/policies/list_component.vue';
 import App from 'ee/security_orchestration/components/policies/app.vue';
@@ -17,6 +20,9 @@ import projectScanExecutionPoliciesQuery from 'ee/security_orchestration/graphql
 import groupScanExecutionPoliciesQuery from 'ee/security_orchestration/graphql/queries/group_scan_execution_policies.query.graphql';
 import projectScanResultPoliciesQuery from 'ee/security_orchestration/graphql/queries/project_scan_result_policies.query.graphql';
 import groupScanResultPoliciesQuery from 'ee/security_orchestration/graphql/queries/group_scan_result_policies.query.graphql';
+import projectPipelineExecutionPoliciesQuery from 'ee/security_orchestration/graphql/queries/project_pipeline_execution_policies.query.graphql';
+import groupPipelineExecutionPoliciesQuery from 'ee/security_orchestration/graphql/queries/group_pipeline_execution_policies.query.graphql';
+import { NAMESPACE_TYPES } from 'ee/security_orchestration/constants';
 import { DEFAULT_PROVIDE } from './mocks';
 
 Vue.use(VueApollo);
@@ -27,11 +33,20 @@ const projectScanExecutionPoliciesSpy = projectScanExecutionPolicies(
 const groupScanExecutionPoliciesSpy = groupScanExecutionPolicies(mockScanExecutionPoliciesResponse);
 const projectScanResultPoliciesSpy = projectScanResultPolicies(mockScanResultPoliciesResponse);
 const groupScanResultPoliciesSpy = groupScanResultPolicies(mockScanResultPoliciesResponse);
+const projectPipelineExecutionPoliciesSpy = projectPipelineResultPolicies(
+  mockPipelineExecutionPoliciesResponse,
+);
+const groupPipelineExecutionPoliciesSpy = groupPipelineResultPolicies(
+  mockPipelineExecutionPoliciesResponse,
+);
+
 const defaultRequestHandlers = {
   projectScanExecutionPolicies: projectScanExecutionPoliciesSpy,
   groupScanExecutionPolicies: groupScanExecutionPoliciesSpy,
   projectScanResultPolicies: projectScanResultPoliciesSpy,
   groupScanResultPolicies: groupScanResultPoliciesSpy,
+  projectPipelineExecutionPolicies: projectPipelineExecutionPoliciesSpy,
+  groupPipelineExecutionPolicies: groupPipelineExecutionPoliciesSpy,
 };
 
 describe('Policies List', () => {
@@ -41,7 +56,7 @@ describe('Policies List', () => {
   const findPoliciesHeader = () => wrapper.findComponent(ListHeader);
   const findPoliciesList = () => wrapper.findComponent(ListComponent);
 
-  const createWrapper = ({ handlers = [] } = {}) => {
+  const createWrapper = ({ handlers = [], provide = {} } = {}) => {
     requestHandlers = {
       ...defaultRequestHandlers,
       ...handlers,
@@ -50,17 +65,20 @@ describe('Policies List', () => {
     wrapper = mount(App, {
       provide: {
         ...DEFAULT_PROVIDE,
+        ...provide,
       },
       apolloProvider: createMockApollo([
         [projectScanExecutionPoliciesQuery, requestHandlers.projectScanExecutionPolicies],
         [groupScanExecutionPoliciesQuery, requestHandlers.groupScanExecutionPolicies],
         [projectScanResultPoliciesQuery, requestHandlers.projectScanResultPolicies],
         [groupScanResultPoliciesQuery, requestHandlers.groupScanResultPolicies],
+        [projectPipelineExecutionPoliciesQuery, requestHandlers.projectPipelineExecutionPolicies],
+        [groupPipelineExecutionPoliciesQuery, requestHandlers.groupPipelineExecutionPolicies],
       ]),
     });
   };
 
-  describe('default', () => {
+  describe('project level with pipelineExecutionPolicyType feature flag off', () => {
     beforeEach(() => {
       window.gon.features = { pipelineExecutionPolicyType: false };
       createWrapper();
@@ -69,6 +87,92 @@ describe('Policies List', () => {
     it('renders the page correctly', () => {
       expect(findPoliciesHeader().exists()).toBe(true);
       expect(findPoliciesList().exists()).toBe(true);
+    });
+
+    it('fetches correct policies on project level', () => {
+      expect(requestHandlers.groupScanResultPolicies).not.toHaveBeenCalled();
+      expect(requestHandlers.groupScanExecutionPolicies).not.toHaveBeenCalled();
+      expect(requestHandlers.groupPipelineExecutionPolicies).not.toHaveBeenCalled();
+
+      expect(requestHandlers.projectScanResultPolicies).toHaveBeenCalled();
+      expect(requestHandlers.projectScanExecutionPolicies).toHaveBeenCalled();
+      expect(requestHandlers.projectPipelineExecutionPolicies).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('project level with pipelineExecutionPolicyType feature flag on', () => {
+    beforeEach(() => {
+      window.gon.features = { pipelineExecutionPolicyType: true };
+      createWrapper({
+        provide: {
+          customCiToggleEnabled: true,
+          glFeatures: {
+            pipelineExecutionPolicyType: true,
+          },
+        },
+      });
+    });
+
+    it('renders the page correctly with ff enabled', () => {
+      expect(findPoliciesHeader().exists()).toBe(true);
+      expect(findPoliciesList().exists()).toBe(true);
+    });
+
+    it('fetches correct policies on project level with ff enabled', () => {
+      expect(requestHandlers.groupScanResultPolicies).not.toHaveBeenCalled();
+      expect(requestHandlers.groupScanExecutionPolicies).not.toHaveBeenCalled();
+      expect(requestHandlers.groupPipelineExecutionPolicies).not.toHaveBeenCalled();
+
+      expect(requestHandlers.projectScanResultPolicies).toHaveBeenCalled();
+      expect(requestHandlers.projectScanExecutionPolicies).toHaveBeenCalled();
+      expect(requestHandlers.projectPipelineExecutionPolicies).toHaveBeenCalled();
+    });
+  });
+
+  describe('group level with pipelineExecutionPolicyType feature flag off', () => {
+    beforeEach(() => {
+      window.gon.features = { pipelineExecutionPolicyType: false };
+      createWrapper({
+        provide: {
+          namespaceType: NAMESPACE_TYPES.GROUP,
+          customCiToggleEnabled: true,
+        },
+      });
+    });
+
+    it('fetches correct policies', () => {
+      expect(requestHandlers.groupScanResultPolicies).toHaveBeenCalled();
+      expect(requestHandlers.groupScanExecutionPolicies).toHaveBeenCalled();
+      expect(requestHandlers.groupPipelineExecutionPolicies).not.toHaveBeenCalled();
+
+      expect(requestHandlers.projectScanResultPolicies).not.toHaveBeenCalled();
+      expect(requestHandlers.projectScanExecutionPolicies).not.toHaveBeenCalled();
+      expect(requestHandlers.projectPipelineExecutionPolicies).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('group level with pipelineExecutionPolicyType feature flag on', () => {
+    beforeEach(() => {
+      window.gon.features = { pipelineExecutionPolicyType: true };
+      createWrapper({
+        provide: {
+          namespaceType: NAMESPACE_TYPES.GROUP,
+          customCiToggleEnabled: true,
+          glFeatures: {
+            pipelineExecutionPolicyType: true,
+          },
+        },
+      });
+    });
+
+    it('fetches correct policies', () => {
+      expect(requestHandlers.groupScanResultPolicies).toHaveBeenCalled();
+      expect(requestHandlers.groupScanExecutionPolicies).toHaveBeenCalled();
+      expect(requestHandlers.groupPipelineExecutionPolicies).toHaveBeenCalled();
+
+      expect(requestHandlers.projectScanResultPolicies).not.toHaveBeenCalled();
+      expect(requestHandlers.projectScanExecutionPolicies).not.toHaveBeenCalled();
+      expect(requestHandlers.projectPipelineExecutionPolicies).not.toHaveBeenCalled();
     });
   });
 });
