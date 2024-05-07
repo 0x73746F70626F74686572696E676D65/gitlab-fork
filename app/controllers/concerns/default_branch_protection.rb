0aecc94aa4aec7efd6cb543bb3ae03ce242276f3
Illya Klymov <iklymov@gitlab.com>
2024-05-07 10:32:48 +0000

    Address reviewer comments
    * improve reliability and clarity

--- app/controllers/concerns/default_branch_protection.rb
+++ app/controllers/concerns/default_branch_protection.rb
@@ -4,31 +4,34 @@ module DefaultBranchProtection
   extend ActiveSupport::Concern
 
   def normalize_default_branch_params!(form_key)
-    target = params[form_key]
+    # The entity being configured will either be the instance or the group
+    entity_settings_params = params[form_key]
 
-    if Gitlab::Utils.to_boolean(target[:default_branch_protected]) == false
-      target[:default_branch_protection_defaults] =
+    if Gitlab::Utils.to_boolean(entity_settings_params[:default_branch_protected]) == false
+      entity_settings_params[:default_branch_protection_defaults] =
         ::Gitlab::Access::BranchProtection.protection_none
+
+      return entity_settings_params
     end
 
-    return target unless target.key?(:default_branch_protection_defaults)
+    return entity_settings_params unless entity_settings_params.key?(:default_branch_protection_defaults)
 
-    target.delete(:default_branch_protection_level)
+    entity_settings_params.delete(:default_branch_protection_level)
 
-    target[:default_branch_protection_defaults][:allowed_to_push].each do |entry|
+    entity_settings_params[:default_branch_protection_defaults][:allowed_to_push].each do |entry|
       entry[:access_level] = entry[:access_level].to_i
     end
 
-    target[:default_branch_protection_defaults][:allowed_to_merge].each do |entry|
+    entity_settings_params[:default_branch_protection_defaults][:allowed_to_merge].each do |entry|
       entry[:access_level] = entry[:access_level].to_i
     end
 
     [:allow_force_push, :code_owner_approval_required, :developer_can_initial_push].each do |key|
-      next unless target[:default_branch_protection_defaults].key?(key)
+      next unless entity_settings_params[:default_branch_protection_defaults].key?(key)
 
-      target[:default_branch_protection_defaults][key] =
+      entity_settings_params[:default_branch_protection_defaults][key] =
         Gitlab::Utils.to_boolean(
-          target[:default_branch_protection_defaults][key],
+          entity_settings_params[:default_branch_protection_defaults][key],
           default: ::Gitlab::Access::BranchProtection.protected_fully[key])
     end
   end
