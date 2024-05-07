48921ddfc806960b1c623d31c4590cc744e461e6
Gabriel Mazetto <gabriel@gitlab.com>
2024-05-07 12:25:59 +0200

    Add specs and documentation to the serializer

--- gems/gitlab-backup-cli/lib/gitlab/backup/cli/metadata/serializer.rb
+++ gems/gitlab-backup-cli/lib/gitlab/backup/cli/metadata/serializer.rb
@@ -12,8 +12,8 @@ module Gitlab
           # JSON primitive type before serializing
           #
           # @param [Symbol] type
-          # @param [Object] value
-          # @return [Object] the converted JSON primitive value
+          # @param [String|Time|Integer] value
+          # @return [Integer, String] the converted JSON primitive value
           def serialize_value(type:, value:)
             return value if value.nil?
 
@@ -22,21 +22,31 @@ module Gitlab
             when :time then serialize_time(value)
             when :integer then serialize_integer(value)
             else
-              raise NameError, "Unknown data type key #{type.inspect} provided when serializing backup metadata"
+              raise ArgumentError, "Unknown data type key #{type.inspect} provided when serializing backup metadata"
             end
           end
 
+          # Serialize the integer value
+          #
+          # @return [Integer]
           def serialize_integer(value)
             return value if value.nil?
 
             value.to_i
           end
 
+          # Serialize the String value
+          #
+          # @return [String]
           def serialize_string(value)
             value.to_s
           end
 
+          # Serialize the Time value using ISO8601 format
+          #
+          # @return [String]
           def serialize_time(value)
+            return value if value.nil?
             raise ArgumentError unless value.is_a?(Time)
 
             # ensures string values and nil are properly cast to Time objects
