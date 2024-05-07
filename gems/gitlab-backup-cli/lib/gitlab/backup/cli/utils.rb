0344d0d7a2e24d16909c238e84d8a760594c0eca
Gabriel Mazetto <gabriel@gitlab.com>
2024-05-07 17:57:11 +0200

    Remove ununsed autoload entry

--- gems/gitlab-backup-cli/lib/gitlab/backup/cli/utils.rb
+++ gems/gitlab-backup-cli/lib/gitlab/backup/cli/utils.rb
@@ -4,7 +4,6 @@ module Gitlab
   module Backup
     module Cli
       module Utils
-        autoload :MetadataSerialization, 'gitlab/backup/cli/utils/metadata_serialization'
         autoload :PgDump, 'gitlab/backup/cli/utils/pg_dump'
         autoload :Tar, 'gitlab/backup/cli/utils/tar'
       end
