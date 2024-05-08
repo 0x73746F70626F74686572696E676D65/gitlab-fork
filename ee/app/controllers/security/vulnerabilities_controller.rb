e12c31d46eeafcd42f9ad55ed9aafd5f922d337a
Savas Vedova <svedova@gitlab.com>
2024-05-08 18:11:01 +0000

    Rework the feature flag logic
    Apply the groupLevelVulnerabilityReportGrouping feature flag logic
    to instance level vulnerability reports as well. It is redundant to
    maintain another feature flag for that level, therefore refactoring
    the code to reuse the same feature flag is a more favorable approach.

--- ee/app/controllers/security/vulnerabilities_controller.rb
+++ ee/app/controllers/security/vulnerabilities_controller.rb
@@ -8,6 +8,7 @@ module Security
     track_govern_activity 'security_vulnerabilities', :index
 
     before_action do
+      push_frontend_feature_flag(:group_level_vulnerability_report_grouping, @user, type: :development)
       push_frontend_feature_flag(:vulnerability_report_advanced_filtering, @user, type: :beta)
     end
 
