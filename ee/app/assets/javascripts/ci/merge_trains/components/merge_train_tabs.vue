6ed7dd206e2b745e688acaf2d3a1fee168d70538
Payton Burdette <pburdette@gitlab.com>
2024-05-08 17:28:28 +0000

    Build active train and merged tabs with basic table

---
+++ ee/app/assets/javascripts/ci/merge_trains/components/merge_train_tabs.vue
@@ -0,0 +1,30 @@
+<script>
+import { GlTab, GlTabs } from '@gitlab/ui';
+
+export default {
+  name: 'MergeTrainTabs',
+  components: {
+    GlTab,
+    GlTabs,
+  },
+};
+</script>
+
+<template>
+  <gl-tabs lazy>
+    <gl-tab
+      :title="s__('Pipelines|Active train')"
+      data-testid="active-trains-tab"
+      @click="$emit('activeTabClicked')"
+    >
+      <slot name="active"></slot>
+    </gl-tab>
+    <gl-tab
+      :title="s__('Pipelines|Merged')"
+      data-testid="merged-trains-tab"
+      @click="$emit('mergedTabClicked')"
+    >
+      <slot name="merged"></slot>
+    </gl-tab>
+  </gl-tabs>
+</template>
--- ee/app/assets/javascripts/ci/merge_trains/merge_trains_app.vue
+++ ee/app/assets/javascripts/ci/merge_trains/merge_trains_app.vue
@@ -1,6 +1,13 @@
 <script>
+import MergeTrainTabs from './components/merge_train_tabs.vue';
+import MergeTrainsTable from './components/merge_trains_table.vue';
+
 export default {
   name: 'MergeTrainsApp',
+  components: {
+    MergeTrainTabs,
+    MergeTrainsTable,
+  },
   inject: {
     fullPath: {
       default: '',
@@ -10,5 +17,16 @@ export default {
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <h1 class="gl-font-size-h1 gl-mb-5">{{ s__('Pipelines|Merge train') }}</h1>
+
+    <merge-train-tabs class="gl-pt-2">
+      <template #active>
+        <merge-trains-table />
+      </template>
+      <template #merged>
+        <merge-trains-table />
+      </template>
+    </merge-train-tabs>
+  </div>
 </template>
