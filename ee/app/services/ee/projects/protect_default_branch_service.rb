ba25cab465f14ccd9da05984d2fb45846a9c4e13
Harsimar Sandhu <hsandhu@gitlab.com>
2024-05-09 08:03:44 +0000

    Add new params in protected branch creation
    This commit adds code_owner_approval_required and allow_force_push
    param in protected branch creation
    
    EE: true
    Changelog: added

--- ee/app/services/ee/projects/protect_default_branch_service.rb
+++ ee/app/services/ee/projects/protect_default_branch_service.rb
@@ -19,6 +19,14 @@ module EE
         super
       end
 
+      override :code_owner_approval_required?
+      def code_owner_approval_required?
+        return false unless ::Feature.enabled?(:default_branch_protection_defaults, project)
+        return false unless ::License.feature_available?(:code_owner_approval_required)
+
+        default_branch_protection.code_owner_approval_required?
+      end
+
       override :merge_access_level
       def merge_access_level
         return ::Gitlab::Access::MAINTAINER if security_policy_management_project?
