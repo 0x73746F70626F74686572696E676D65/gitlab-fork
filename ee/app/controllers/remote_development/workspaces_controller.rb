86b6e75580f09fe3b6a550d74f6f296378bce184
Chad Woolley <cwoolley@gitlab.com>
2024-05-13 08:52:26 +0000

    Fix workspaces frontend auth feature flag
    - Needs to use group actor, not user

--- ee/app/controllers/remote_development/workspaces_controller.rb
+++ ee/app/controllers/remote_development/workspaces_controller.rb
@@ -3,9 +3,6 @@
 module RemoteDevelopment
   class WorkspacesController < ApplicationController
     before_action :authorize_remote_development!, only: [:index]
-    before_action do
-      push_frontend_feature_flag(:remote_development_namespace_agent_authorization, current_user)
-    end
 
     feature_category :remote_development
     urgency :low
