4e12e0027592cb573bc059c8b11c209edb781b4a
Marcos Rocha <mrocha@gitlab.com>
2024-05-13 17:40:51 +0000

    Add custom software licenses table
    This MR adds a new table custom_software_licenses to
    stores the software licenses that are not part of
    the SPDX. These licenses will be used by security
    policies.
    
    Changelog: added
    EE: true

---
+++ ee/spec/models/security/custom_software_license_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Security::CustomSoftwareLicense, feature_category: :security_policy_management do
+  describe 'associations' do
+    it { is_expected.to belong_to(:project) }
+  end
+
+  describe 'validations' do
+    subject(:custom_software_license) { build(:custom_software_license) }
+
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_most(255) }
+    it { is_expected.to validate_presence_of(:project_id) }
+    it { is_expected.to(validate_uniqueness_of(:name).scoped_to(%i[project_id])) }
+  end
+end
