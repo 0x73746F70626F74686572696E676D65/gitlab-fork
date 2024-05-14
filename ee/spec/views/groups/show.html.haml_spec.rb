22743902a07bceb448dd74ff9a386ee4e89c12cb
Ross Byrne <robyrne@gitlab.com>
2024-05-14 11:46:01 +0000

    Clean up duo_pro_trial_alert feature flag
    Removes feature flag and the alert
    
    Changelog: changed
    EE: true

--- ee/spec/views/groups/show.html.haml_spec.rb
+++ ee/spec/views/groups/show.html.haml_spec.rb
@@ -9,12 +9,6 @@ RSpec.describe 'groups/show', feature_category: :groups_and_projects do
     assign(:group, group)
   end
 
-  it 'renders the Duo Pro trial alert partial' do
-    render
-
-    expect(rendered).to render_template('shared/_duo_pro_trial_alert')
-  end
-
   it 'renders the Duo Chat GA alert partial' do
     render
 
