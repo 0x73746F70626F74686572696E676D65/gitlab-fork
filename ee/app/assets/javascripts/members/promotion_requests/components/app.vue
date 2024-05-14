5ed8787d0e678b43582f44ea6910450b5ed21884
Ammar Alakkad <am.alakkad@gmail.com>
2024-05-14 18:38:55 +0300

    Show pending promotion users in table

--- ee/app/assets/javascripts/members/promotion_requests/components/app.vue
+++ ee/app/assets/javascripts/members/promotion_requests/components/app.vue
@@ -1,11 +1,42 @@
 <script>
 // eslint-disable-next-line no-restricted-imports
 import { mapState } from 'vuex';
+import { GlTableLite, GlAvatarLabeled, GlAvatarLink } from '@gitlab/ui';
+import { s__ } from '~/locale';
 import MembersPagination from '~/members/components/table/members_pagination.vue';
+import UserDate from '~/vue_shared/components/user_date.vue';
+
+export const FIELDS = [
+  {
+    key: 'user',
+    label: s__('Members|User'),
+  },
+  {
+    key: 'requested_role',
+    label: s__('Members|Requested Role'),
+    tdClass: '!gl-align-middle',
+  },
+  {
+    key: 'requested_by',
+    label: s__('Members|Requested By'),
+    tdClass: '!gl-align-middle',
+  },
+  {
+    key: 'requested_on',
+    label: s__('Members|Requested On'),
+    tdClass: '!gl-align-middle',
+  },
+];
 
 export default {
   name: 'PromotionRequestsTabApp',
-  components: { MembersPagination },
+  components: {
+    MembersPagination,
+    GlAvatarLabeled,
+    GlAvatarLink,
+    GlTableLite,
+    UserDate,
+  },
   props: {
     namespace: {
       type: String,
@@ -18,7 +49,7 @@ export default {
   },
   computed: {
     ...mapState({
-      data(state) {
+      users(state) {
         return state[this.namespace].data;
       },
       pagination(state) {
@@ -26,15 +57,32 @@ export default {
       },
     }),
   },
+  FIELDS,
 };
 </script>
 <template>
   <div>
-    <ul>
-      <li v-for="request in data" :key="request.id">
-        {{ request.user.name }}
-      </li>
-    </ul>
+    <gl-table-lite :items="users" :fields="$options.FIELDS">
+      <template #cell(user)="{ item: { user } }">
+        <gl-avatar-link target="blank" :href="user.web_url">
+          <gl-avatar-labeled
+            :src="user.avatar_url"
+            :size="32"
+            :label="user.name"
+            :sub-label="user.username"
+          />
+        </gl-avatar-link>
+      </template>
+      <template #cell(requested_role)="{ item }">
+        {{ item.newAccessLevel.stringValue }}
+      </template>
+      <template #cell(requested_by)="{ item }">
+        <a :href="item.requestedBy.webUrl">{{ item.requestedBy.name }}</a>
+      </template>
+      <template #cell(requested_on)="{ item }">
+        <user-date :date="item.createdAt" />
+      </template>
+    </gl-table-lite>
     <members-pagination :pagination="pagination" :tab-query-param-value="tabQueryParamValue" />
   </div>
 </template>
--- ee/spec/frontend/members/promotion_requests/components/app_spec.js
+++ ee/spec/frontend/members/promotion_requests/components/app_spec.js
@@ -1,11 +1,13 @@
 import Vue, { nextTick } from 'vue';
 // eslint-disable-next-line no-restricted-imports
 import Vuex from 'vuex';
+import { GlTableLite } from '@gitlab/ui';
 import { mountExtended } from 'helpers/vue_test_utils_helper';
 import PromotionRequestsApp from 'ee/members/promotion_requests/components/app.vue';
 import { MEMBER_TYPES, TAB_QUERY_PARAM_VALUES } from 'ee_else_ce/members/constants';
 import initStore from 'ee/members/promotion_requests/store/index';
 import MembersPagination from '~/members/components/table/members_pagination.vue';
+import UserDate from '~/vue_shared/components/user_date.vue';
 import { data as mockData, pagination as mockPagination } from '../mock_data';
 
 describe('PromotionRequestsApp', () => {
@@ -26,20 +28,31 @@ describe('PromotionRequestsApp', () => {
         namespace: MEMBER_TYPES.promotionRequest,
         tabQueryParamValue: TAB_QUERY_PARAM_VALUES.promotionRequest,
       },
+      provide: { canManageMembers: false },
       store,
     });
 
     return nextTick();
   };
 
+  const findTable = () => wrapper.findComponent(GlTableLite);
+
   beforeEach(async () => {
     await createComponent();
   });
 
-  it('will render the list of users pending promotion', () => {
-    const list = wrapper.findAll('li');
-    expect(list.length).toBe(mockData.length);
-    expect(wrapper.text()).toContain(mockData[0].user.name);
+  it('renders the table of users pending promotion', () => {
+    expect(findTable().exists()).toBe(true);
+
+    const rows = findTable().findAll('tbody > tr');
+    expect(rows.length).toEqual(mockData.length);
+
+    const columns = rows.at(0).findAll('td');
+    expect(columns.at(0).text()).toContain(mockData[0].user.name);
+    expect(columns.at(1).text()).toBe(mockData[0].newAccessLevel.stringValue);
+    expect(columns.at(2).text()).toBe(mockData[0].requestedBy.name);
+    expect(columns.at(3).findComponent(UserDate).exists()).toBe(true);
+    expect(columns.at(3).findComponent(UserDate).props('date')).toBe(mockData[0].createdAt);
   });
 
   it('renders `members-pagination` component with correct props', () => {
