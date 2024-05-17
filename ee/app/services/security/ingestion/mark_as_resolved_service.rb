6d123e6b2ecde551a33832937ebe0d2f4faf8fdf
Olivier Gonzalez <52092-gonzoyumo@users.noreply.gitlab.com>
2024-05-17 18:12:39 +0000

    Mark CVS vulnerabilities as no longer detected

--- ee/app/services/security/ingestion/mark_as_resolved_service.rb
+++ ee/app/services/security/ingestion/mark_as_resolved_service.rb
@@ -7,6 +7,10 @@ module Security
     # detected by the same scanner, and marks them as resolved
     # on the default branch if they were not detected again.
     class MarkAsResolvedService
+      CVS_SCANNER_EXTERNAL_ID = 'gitlab-sbom-vulnerability-scanner'
+      CS_SCANNERS_EXTERNAL_IDS = %w[trivy].freeze
+      DS_SCANNERS_EXTERNAL_IDS = %w[gemnasium gemnasium-maven gemnasium-python].freeze
+
       def self.execute(scanner, ingested_ids)
         new(scanner, ingested_ids).execute
       end
@@ -22,6 +26,14 @@ module Security
         vulnerability_reads
           .by_scanner(scanner)
           .each_batch { |batch| process_batch(batch) }
+
+        return unless Feature.enabled?(:mark_cvs_vulnerabilities_as_resolved, project)
+
+        if scanner_for_container_scanning?
+          process_existing_cvs_vulnerabilities_for_container_scanning
+        elsif scanner_for_dependency_scanning?
+          process_existing_cvs_vulnerabilities_for_dependency_scanning
+        end
       end
 
       private
@@ -42,6 +54,34 @@ module Security
                      .with_resolution(false)
                      .not_requiring_manual_resolution
                      .update_all(resolved_on_default_branch: true)
+      end
+
+      def process_existing_cvs_vulnerabilities_for_container_scanning
+        vulnerability_reads
+          .by_scanner_ids(cvs_scanner_id)
+          .with_report_types(:container_scanning)
+          .each_batch { |batch| process_batch(batch) }
+      end
+
+      def process_existing_cvs_vulnerabilities_for_dependency_scanning
+        vulnerability_reads
+          .by_scanner_ids(cvs_scanner_id)
+          .with_report_types(:dependency_scanning)
+          .each_batch { |batch| process_batch(batch) }
+      end
+
+      def cvs_scanner_id
+        ::Vulnerabilities::Scanner.for_projects(project.id)
+          .with_external_id(CVS_SCANNER_EXTERNAL_ID)
+          .pluck_primary_key
+      end
+
+      def scanner_for_container_scanning?
+        scanner.external_id.in?(CS_SCANNERS_EXTERNAL_IDS)
+      end
+
+      def scanner_for_dependency_scanning?
+        scanner.external_id.in?(DS_SCANNERS_EXTERNAL_IDS)
       end
     end
   end
