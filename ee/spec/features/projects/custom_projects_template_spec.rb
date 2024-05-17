49c9b2dbd25e37209cbdacb700dc9856e062c257
Arturo Herrero <arturo.herrero@gmail.com>
2024-05-17 01:47:00 +0000

    Update feature category for custom projects template spec

--- ee/spec/features/projects/custom_projects_template_spec.rb
+++ ee/spec/features/projects/custom_projects_template_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe 'Project', feature_category: :groups_and_projects do
+RSpec.describe 'Project', feature_category: :source_code_management do
   describe 'Custom instance-level projects templates' do
     let_it_be(:user) { create(:user) }
     let_it_be(:group) { create(:group) }
--- ee/spec/features/projects/new_project_from_template_spec.rb
+++ ee/spec/features/projects/new_project_from_template_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe 'New project from template', :js, feature_category: :groups_and_projects do
+RSpec.describe 'New project from template', :js, feature_category: :source_code_management do
   let(:user) { create(:user) }
 
   before do
