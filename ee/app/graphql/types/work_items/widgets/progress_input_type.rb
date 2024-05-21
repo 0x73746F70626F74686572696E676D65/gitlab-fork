07a5bb742f59352cb1eff205b50bd3f342b4c6e0
Gaurav Marwal <gauravmarwal@gmail.com>
2024-05-21 13:45:04 +0000

    Resolve Layout/ArgumentAlignment offenses 49
    Changelog: other
    EE: true

--- ee/app/graphql/types/work_items/widgets/progress_input_type.rb
+++ ee/app/graphql/types/work_items/widgets/progress_input_type.rb
@@ -7,19 +7,19 @@ module Types
         graphql_name 'WorkItemWidgetProgressInput'
 
         argument :current_value,
-                 GraphQL::Types::Int,
-                 required: true,
-                 description: 'Current progress value of the work item.'
+          GraphQL::Types::Int,
+          required: true,
+          description: 'Current progress value of the work item.'
 
         argument :start_value,
-                  GraphQL::Types::Int,
-                  required: false,
-                  description: 'Start value of the work item.'
+          GraphQL::Types::Int,
+          required: false,
+          description: 'Start value of the work item.'
 
         argument :end_value,
-                  GraphQL::Types::Int,
-                  required: false,
-                  description: 'End value of the work item.'
+          GraphQL::Types::Int,
+          required: false,
+          description: 'End value of the work item.'
       end
     end
   end
