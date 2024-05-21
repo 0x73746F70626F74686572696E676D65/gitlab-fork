09179f18e24336f4b1fbc947edb5d92825bda25b
Nicolas Dular <ndular@gitlab.com>
2024-05-21 14:44:38 +0200

    Remove synced_work_item param
    Removes the synced_work_item param from `WorkItems::UpdateService` and
    `WorkItems::CreateService`. This param was necessary when we created the
    work item from the epic services through the Create/Update service of
    the work item. With the param we for example skipped creation of notes
    on the Work Item side.
    
    We now directly use ActiveRecord to create a work item in the
    Epic service and therefore no longer need to skip anything in the
    WorkItem services.

--- ee/app/services/ee/work_items/create_service.rb
+++ ee/app/services/ee/work_items/create_service.rb
@@ -20,43 +20,6 @@ module EE
           create_epic_for!(work_item)
         end
       end
-
-      override :iid_param_allowed?
-      def iid_param_allowed?
-        sync_work_item? || super
-      end
-
-      override :filter_timestamp_params
-      def filter_timestamp_params
-        return if sync_work_item?
-
-        super
-      end
-
-      override :skip_system_notes?
-      def skip_system_notes?
-        return true if sync_work_item?
-
-        super
-      end
-
-      override :after_commit_tasks
-      def after_commit_tasks(user, work_item)
-        return if sync_work_item?
-
-        super
-      end
-
-      override :publish_event
-      def publish_event(work_item)
-        return if sync_work_item?
-
-        super
-      end
-
-      def sync_work_item?
-        extra_params&.fetch(:synced_work_item, false)
-      end
     end
   end
 end
--- ee/app/services/ee/work_items/update_service.rb
+++ ee/app/services/ee/work_items/update_service.rb
@@ -11,7 +11,6 @@ module EE
       attr_reader :widget_params, :callbacks
 
       def transaction_update(work_item, opts = {})
-        return super if sync_work_item?
         return super unless work_item.epic_work_item?
 
         super.tap do |save_result|
@@ -19,41 +18,6 @@ module EE
 
           update_epic_for!(work_item)
         end
-      end
-
-      override :create_system_notes
-      def create_system_notes(issuable, **options)
-        super unless sync_work_item?
-      end
-
-      override :filter_timestamp_params
-      def filter_timestamp_params
-        super unless sync_work_item?
-      end
-
-      override :assign_last_edited
-      def assign_last_edited(work_item)
-        return super unless sync_work_item?
-
-        work_item.assign_attributes(last_edited_at: params[:last_edited_at], last_edited_by: params[:last_edited_by])
-      end
-
-      override :handle_confidential_change
-      def handle_confidential_change(work_item)
-        # We don't want to delete todos, or create a confidential note as part of a synced work item update.
-        # Once the work item is synced from the epic, we want to re-assign the notes and todos to the new work item.
-        super unless sync_work_item?
-      end
-
-      override :can_set_confidentiality?
-      def can_set_confidentiality?(work_item)
-        return true if sync_work_item?
-
-        super
-      end
-
-      def sync_work_item?
-        extra_params&.fetch(:synced_work_item, false)
       end
     end
   end
