46ead44a7a068077733639c59956dd7e5c95b3f9
Siddharth Dungarwal <sdungarwal@gitlab.com>
2024-05-27 14:28:44 +0000

    Mark 20230908161822 migration obsolete

--- ee/spec/elastic/migrate/20230908161822_reindex_work_item_to_fix_label_ids_spec.rb
+++ ee/spec/elastic/migrate/20230908161822_reindex_work_item_to_fix_label_ids_spec.rb
@@ -3,13 +3,6 @@
 require 'spec_helper'
 require File.expand_path('ee/elastic/migrate/20230908161822_reindex_work_item_to_fix_label_ids.rb')
 
-RSpec.describe ReindexWorkItemToFixLabelIds, :elastic_delete_by_query, :sidekiq_inline, feature_category: :global_search do
-  let(:version) { 20230908161822 }
-
-  include_examples 'migration reindex based on schema_version' do
-    let_it_be(:project) { create(:project, archived: true) }
-    let(:objects) { create_list(:work_item, 3, project: project) }
-    let(:expected_throttle_delay) { 1.minute }
-    let(:expected_batch_size) { 9000 }
-  end
+RSpec.describe ReindexWorkItemToFixLabelIds, feature_category: :global_search do
+  it_behaves_like 'a deprecated Advanced Search migration', 20230908161822
 end
