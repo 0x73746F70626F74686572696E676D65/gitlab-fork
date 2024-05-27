1e0bebb699a691764be2f67ca5d6eb2c70729e45
Ezekiel Kigbo <3397881-ekigbo@users.noreply.gitlab.com>
2024-05-27 22:32:44 +0000

    Remove legacy vsd tracking code

--- ee/app/controllers/groups/analytics/dashboards_controller.rb
+++ ee/app/controllers/groups/analytics/dashboards_controller.rb
@@ -5,12 +5,6 @@ module Groups
     class DashboardsController < Groups::Analytics::ApplicationController
       include ProductAnalyticsTracking
 
-      track_event :value_streams_dashboard,
-        name: 'g_metrics_comparison_page',
-        action: 'perform_analytics_usage_action',
-        label: 'redis_hll_counters.analytics.g_metrics_comparison_page_monthly',
-        destinations: %i[redis_hll snowplow]
-
       before_action { authorize_view_by_action!(:read_group_analytics_dashboards) }
       before_action do
         push_frontend_feature_flag(:ai_impact_analytics_dashboard, @group)
@@ -20,20 +14,6 @@ module Groups
       end
 
       layout 'group'
-
-      private
-
-      def tracking_namespace_source
-        @group
-      end
-
-      def tracking_project_source
-        nil
-      end
-
-      def track_value_streams_event
-        Gitlab::InternalEvents.track_event('value_streams_dashboard_viewed', namespace: @group, user: current_user)
-      end
     end
   end
 end
