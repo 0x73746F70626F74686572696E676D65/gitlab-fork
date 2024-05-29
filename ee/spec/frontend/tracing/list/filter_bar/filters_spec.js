0bbf823f231feaff438cbd1f30ac97729545ad67
Daniele Rossetti <drossetti@gitlab.com>
2024-05-29 19:06:26 +0000

    Replace Tracing Period filter with Datepicker component

--- ee/spec/frontend/tracing/list/filter_bar/filters_spec.js
+++ ee/spec/frontend/tracing/list/filter_bar/filters_spec.js
@@ -8,7 +8,6 @@ import {
 describe('utils', () => {
   const query =
     'sortBy=timestamp_desc' +
-    '&period[]=1h' +
     '&service[]=accountingservice&not%5Bservice%5D[]=adservice' +
     '&operation[]=orders%20receive&not%5Boperation%5D[]=orders%20receive' +
     '&gt%5BdurationMs%5D[]=100&lt%5BdurationMs%5D[]=1000' +
@@ -16,11 +15,13 @@ describe('utils', () => {
     '&not%5Btrace_id%5D[]=9609bf00-4b68-f86c-abe2-5e23d0089c83' +
     '&attribute[]=foo%3Dbar&attribute[]=baz%3Dbar' +
     '&status[]=ok&not%5Bstatus%5D[]=error' +
-    '&search=searchquery';
+    '&search=searchquery' +
+    '&date_range=custom' +
+    '&date_end=2020-01-02T00%3A00%3A00.000Z' +
+    '&date_start=2020-01-01T00%3A00%3A00.000Z';
 
   const filterObj = {
     attributes: {
-      period: [{ operator: '=', value: '1h' }],
       service: [
         { operator: '=', value: 'accountingservice' },
         { operator: '!=', value: 'adservice' },
@@ -47,6 +48,11 @@ describe('utils', () => {
       ],
       search: [{ value: 'searchquery' }],
     },
+    dateRange: {
+      value: 'custom',
+      startDate: new Date('2020-01-01'),
+      endDate: new Date('2020-01-02'),
+    },
   };
 
   const queryObj = {
@@ -57,20 +63,20 @@ describe('utils', () => {
     'not[attribute]': null,
     'not[durationMs]': null,
     'not[operation]': ['orders receive'],
-    'not[period]': null,
     'not[service]': ['adservice'],
     'not[trace_id]': ['9609bf00-4b68-f86c-abe2-5e23d0089c83'],
     'not[status]': ['error'],
     operation: ['orders receive'],
-    period: ['1h'],
     status: ['ok'],
     search: 'searchquery',
     service: ['accountingservice'],
     trace_id: ['9609bf00-4b68-f86c-abe2-5e23d0089c83'],
+    date_range: 'custom',
+    date_end: '2020-01-02T00:00:00.000Z',
+    date_start: '2020-01-01T00:00:00.000Z',
   };
 
   const attributesFilterTokens = [
-    { type: 'period', value: { data: '1h', operator: '=' } },
     { type: 'service-name', value: { data: 'accountingservice', operator: '=' } },
     { type: 'service-name', value: { data: 'adservice', operator: '!=' } },
     { type: 'operation', value: { data: 'orders receive', operator: '=' } },
@@ -96,37 +102,6 @@ describe('utils', () => {
     it('should build a filter obj', () => {
       expect(queryToFilterObj(query)).toEqual(filterObj);
     });
-
-    it('should add the default period filter if not specified', () => {
-      expect(queryToFilterObj('service[]=accountingservice')).toEqual({
-        attributes: {
-          period: [{ operator: '=', value: '1h' }],
-          service: [{ operator: '=', value: 'accountingservice' }],
-        },
-      });
-    });
-
-    it.each(['5m', '15m', '30m', '1h', '4h', '12h', '24h', '7d', '2024-02-15 - 2024-02-22'])(
-      'handles valid periods',
-      (value) => {
-        expect(queryToFilterObj(`period[]=${value}`)).toEqual({
-          attributes: {
-            period: [{ operator: '=', value }],
-          },
-        });
-      },
-    );
-
-    it.each(['30d', 'foo', 'foo - bar', '123-456', '2024-02-01 - 2024-02-20'])(
-      'ignores invalid period',
-      (val) => {
-        expect(queryToFilterObj(`period[]=${val}`)).toEqual({
-          attributes: {
-            period: [{ operator: '=', value: '1h' }],
-          },
-        });
-      },
-    );
   });
 
   describe('filterObjToQuery', () => {
@@ -144,15 +119,6 @@ describe('utils', () => {
   describe('filterTokensToFilterObj', () => {
     it('should convert filter tokens to filter object', () => {
       expect(filterTokensToFilterObj(attributesFilterTokens)).toEqual(filterObj.attributes);
-    });
-
-    it('should add the default period filter it not specified', () => {
-      expect(
-        filterTokensToFilterObj([{ type: 'duration-ms', value: { data: '100', operator: '>' } }]),
-      ).toEqual({
-        period: [{ operator: '=', value: '1h' }],
-        durationMs: [{ operator: '>', value: '100' }],
-      });
     });
   });
 });
