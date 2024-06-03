cf12d7d1d24bc453f4ae0b9a3a40b1461bc969e7
Oscar Tovar <otovar@gitlab.com>
2024-06-03 16:51:47 -0400

    Upgrade semver_dialects gem from 2.0.2 to 3.0.0
    This improves the accuracy of Continuous Vulnerability Scanning and
    License Scanning when comparing the version of a SBOM component to the
    advisory database and the license database, respectively.
    
    Additionally, this changes the error handling of raised errors from
    license scanning so that they're always sent to Sentry as recommended by
    the developer documentation. Previously, the messages were only logged
    which meant that after the log TTL passed, we'd be left with a very hard
    to debug situation as was the case with the initial update to v3.0.0.
    Sentry does not have the same restrictions, so this improves our ability
    to debug any increase in the error rate.
    
    Lastly, the error tracking now includes, the following information that
    provides more detail into what went wrong:
    
    * The version of the package being checked
    * The range of the licenses known - lowest and highest version strings
    * The package name and purl type
    
    This means that we can check the license exports directly to find invalid
    constraints in the exports instead of requiring access to the instance
    database.

--- ee/spec/models/package_metadata/package_spec.rb
+++ ee/spec/models/package_metadata/package_spec.rb
@@ -54,6 +54,37 @@ RSpec.describe PackageMetadata::Package, type: :model, feature_category: :softwa
         end
       end
 
+      context 'and the PURL type is supported' do
+        context 'and the input version matches the default licenses' do
+          let(:license_ids) { package.license_ids_for(version: input_version) }
+
+          where(:purl_type, :input_version) do
+            'composer' | '2.2.2'
+            'conan' | '2.2.2'
+            'gem' | '2.2.1.rc.1'
+            'golang' | '2.2.2-alpha1'
+            'maven' | '2.6a1'
+            'npm' | '2.2.2-alpha1'
+            'nuget' | '2.2.2-alpha1'
+            'pypi' | '1.11-dev1'
+          end
+
+          with_them do
+            let(:package) do
+              build_stubbed(:pm_package, name: "cliui", purl_type: purl_type,
+                licenses: [default, lowest, highest, other])
+            end
+
+            let(:lowest) { '0.0.0' }
+            let(:highest) { input_version }
+
+            subject(:license_ids) { package.license_ids_for(version: input_version) }
+
+            specify { expect(license_ids).to eq(default) }
+          end
+        end
+      end
+
       context 'and the given version causes semver_dialects to raise an exception while parsing' do
         let(:package) do
           build_stubbed(:pm_package, name: "cliui", purl_type: "npm", licenses: [default, lowest, highest, other])
@@ -63,13 +94,17 @@ RSpec.describe PackageMetadata::Package, type: :model, feature_category: :softwa
 
         subject(:license_ids) { package.license_ids_for(version: input_version) }
 
-        it 'logs the exception' do
+        it 'tracks the exception' do
           log_params = {
-            id: package.id, version: input_version, message: "semver_dialects parse error",
+            id: package.id,
+            package_name: 'cliui',
+            purl_type: 'npm',
+            version: input_version, lowest_version: lowest, highest_version: highest,
+            message: "semver_dialects parse error",
             error: a_string_including("invalid constraint")
           }
 
-          expect(Gitlab::ErrorTracking).to receive(:log_exception)
+          expect(Gitlab::ErrorTracking).to receive(:track_exception)
             .with(an_instance_of(SemverDialects::InvalidConstraintError), **log_params)
             .and_call_original
 
