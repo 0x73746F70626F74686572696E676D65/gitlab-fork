ea7ca973aa1f8e75d6bbb74fbf9e4f3343c5e954
gdk <gdk@example.com>
2024-06-03 03:13:32 +0000

    Quarantine a flaky test
    The `Types::MemberRoles::PermissionsEnum exposes all the existing custom permissions`
    test has either ~"flakiness::1" or ~"flakiness::2" label set, which means the number of reported failures
    is at or above 95 percentile, indicating unusually high failure count.
    
    This MR quarantines the test. This is a discussion starting point to let the
    responsible group know about the flakiness so that they can take action:
    
    - accept the merge request and schedule the associated issue to improve the test
    - close the merge request in favor of another merge request to delete the test
    
    Please follow the
    [Flaky tests management process](https://handbook.gitlab.com/handbook/engineering/infrastructure/engineering-productivity/flaky-tests-management-and-processes/#flaky-tests-management-process)
    to help us increase `master` stability.
    
    Please let us know your feedback
    [in the dedicated issue](https://gitlab.com/gitlab-org/quality/engineering-productivity/team/-/issues/447).
    
    Related to https://gitlab.com/gitlab-org/gitlab/-/issues/449111.
    
    This change was generated by
    [gitlab-housekeeper](https://gitlab.com/gitlab-org/gitlab/-/tree/master/gems/gitlab-housekeeper)
    using the Keeps::QuarantineFlakyTests keep.
    
    To provide feedback on your experience with `gitlab-housekeeper` please comment in
    <https://gitlab.com/gitlab-org/gitlab/-/issues/442003>.
    
    Changelog: other

--- ee/spec/graphql/types/member_roles/permissions_enum_spec.rb
+++ ee/spec/graphql/types/member_roles/permissions_enum_spec.rb
@@ -5,7 +5,8 @@ require 'spec_helper'
 RSpec.describe GitlabSchema.types['MemberRolePermission'], feature_category: :system_access do
   specify { expect(described_class.graphql_name).to eq('MemberRolePermission') }
 
-  it 'exposes all the existing custom permissions' do
+  it 'exposes all the existing custom permissions',
+    quarantine: 'https://gitlab.com/gitlab-org/gitlab/-/issues/449111' do
     expect(described_class.values.keys)
       .to match_array(::MemberRole.all_customizable_permissions.keys.map(&:to_s).map(&:upcase))
   end
