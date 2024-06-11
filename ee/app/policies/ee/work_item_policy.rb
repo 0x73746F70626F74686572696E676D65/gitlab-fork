0d77a05dd638c2acb95cf6aa9458b5cf14319311
Joseph Wambua <jjoshua@gitlab.com>
2024-06-11 16:58:40 +0300

    Check for related_epics ff when linking epics
    When linking epic work items, we need to check whether the related_epics
    feature flag is enabled in order to allow the admin_work_item_link
    policy.
    
    Changelog: changed
    EE: true

--- ee/app/policies/ee/work_item_policy.rb
+++ ee/app/policies/ee/work_item_policy.rb
@@ -10,9 +10,18 @@ module EE
     end
 
     prepended do
+      condition(:is_epic, scope: :subject) do
+        @subject.work_item_type&.epic?
+      end
+      condition(:related_epics_available, scope: :subject) do
+        @subject.namespace.licensed_feature_available?(:related_epics)
+      end
+
       rule { has_synced_epic }.policy do
         prevent(*synced_work_item_disallowed_abilities)
       end
+
+      rule { is_epic & ~related_epics_available }.prevent :admin_work_item_link
     end
   end
 end
