291ab9e38dbf392199b512c32644254ce126cc11
Pavel Shutsin <pshutsin@gitlab.com>
2024-06-11 13:57:47 +0200

    Improve usage data specs
    Pure spec coverage improvement

--- ee/spec/requests/api/usage_data_spec.rb
+++ ee/spec/requests/api/usage_data_spec.rb
@@ -8,32 +8,48 @@ RSpec.describe API::UsageData, feature_category: :service_ping do
   describe 'POST /usage_data/track_event' do
     let(:endpoint) { '/usage_data/track_event' }
 
-    context 'with usage ping enabled' do
-      before do
-        stub_application_setting(usage_ping_enabled: true)
+    before do
+      stub_application_setting(usage_ping_enabled: true, use_clickhouse_for_analytics: true)
+    end
+
+    context 'with AI related metric' do
+      let_it_be(:additional_properties) do
+        {
+          language: 'ruby',
+          timestamp: '2024-01-01',
+          unrelated_info: 'bar'
+        }
       end
 
-      context 'with AI related metric' do
-        let_it_be(:additional_properties) do
-          {
-            language: 'ruby',
-            timestamp: '2024-01-01',
-            unrelated_info: 'bar'
-          }
-        end
+      let(:event_name) { 'code_suggestion_shown_in_ide' }
 
-        let(:event_name) { 'code_suggestion_shown_in_ide' }
+      before do
+        stub_feature_flags(track_ai_metrics_in_usage_data: true)
+      end
 
+      it 'triggers AI tracking' do
+        expect(Gitlab::Tracking::AiTracking).to receive(:track_event)
+                                                  .with(
+                                                    event_name,
+                                                    additional_properties.merge(user: user)
+                                                  ).and_call_original
+
+        post api(endpoint, user), params: {
+          event: event_name,
+          additional_properties: additional_properties
+        }
+
+        expect(response).to have_gitlab_http_status(:ok)
+      end
+
+      context 'with transition approach' do
         before do
-          stub_feature_flags(track_ai_metrics_in_usage_data: true)
+          allow(Gitlab::Tracking::AiTracking)
+            .to receive(:track_via_code_suggestions?).with(event_name, anything).and_return(true)
         end
 
-        it 'triggers AI tracking' do
-          expect(Gitlab::Tracking::AiTracking).to receive(:track_event)
-                                              .with(
-                                                event_name,
-                                                additional_properties.merge(user: user)
-                                              ).and_call_original
+        it 'does not trigger AI tracking' do
+          expect(Gitlab::Tracking::AiTracking).not_to receive(:track_event)
 
           post api(endpoint, user), params: {
             event: event_name,
@@ -41,24 +57,6 @@ RSpec.describe API::UsageData, feature_category: :service_ping do
           }
 
           expect(response).to have_gitlab_http_status(:ok)
-        end
-
-        context 'with transition approach' do
-          before do
-            allow(Gitlab::Tracking::AiTracking)
-              .to receive(:track_via_code_suggestions?).with(event_name, anything).and_return(true)
-          end
-
-          it 'does not trigger AI tracking' do
-            expect(Gitlab::Tracking::AiTracking).not_to receive(:track_event)
-
-            post api(endpoint, user), params: {
-              event: event_name,
-              additional_properties: additional_properties
-            }
-
-            expect(response).to have_gitlab_http_status(:ok)
-          end
         end
       end
     end
