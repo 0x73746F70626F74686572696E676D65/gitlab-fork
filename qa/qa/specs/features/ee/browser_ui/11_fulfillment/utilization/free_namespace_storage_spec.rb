4bf7a427d3d37cbdb6bb29deaa127b9f110cd76f
Valerie Burton <vburton@gitlab.com>
2024-06-12 06:20:10 -0500

    E2E: Quarantine free_namespace_storage_spec

--- qa/qa/specs/features/ee/browser_ui/11_fulfillment/utilization/free_namespace_storage_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/11_fulfillment/utilization/free_namespace_storage_spec.rb
@@ -4,7 +4,11 @@ module QA
   RSpec.describe 'Fulfillment', :runner, :requires_admin,
     only: { subdomain: :staging },
     feature_flag: { name: 'namespace_storage_limit', scope: :group },
-    product_group: :utilization do
+    product_group: :utilization,
+    quarantine: {
+      issue: 'https://gitlab.com/gitlab-org/gitlab/-/issues/466675',
+      type: :flaky
+    } do
     describe 'Utilization' do
       include Runtime::Fixtures
 
