1dd6c04c6aa50c501efce149dc5adf4bf78ba165
Joe Woodward <j@joewoodward.me>
2024-06-13 14:58:11 +0100

    Improve ProtectedRefAccess#type logic
    Prior to this change we only checked if user or group was present. This
    is not sufficient to ensure the correct type is returned. If a
    ProtectedRefAccess level has a user_id or group_id assigned and the user
    or group is not loaded (or doesn't exist) then :role would be returned
    incorrectly.
    
    This change checks both user_id and user, and group_id and group when
    inferring the type.

--- ee/spec/models/protected_branch/push_access_level_spec.rb
+++ ee/spec/models/protected_branch/push_access_level_spec.rb
@@ -3,6 +3,7 @@
 require 'spec_helper'
 
 RSpec.describe ProtectedBranch::PushAccessLevel, feature_category: :source_code_management do
-  include_examples 'protected ref access configured for users', :protected_branch
-  include_examples 'ee protected branch access'
+  it_behaves_like 'protected ref access configured for users', :protected_branch
+  it_behaves_like 'ee protected ref access', :protected_branch
+  it_behaves_like 'ee protected branch access'
 end
--- ee/spec/models/protected_branch/unprotect_access_level_spec.rb
+++ ee/spec/models/protected_branch/unprotect_access_level_spec.rb
@@ -3,8 +3,9 @@
 require 'spec_helper'
 
 RSpec.describe ProtectedBranch::UnprotectAccessLevel, feature_category: :source_code_management do
-  include_examples 'protected branch access'
-  include_examples 'protected ref access allowed_access_levels', excludes: [Gitlab::Access::NO_ACCESS]
-  include_examples 'protected ref access configured for users', :protected_branch
-  include_examples 'protected ref access configured for groups', :protected_branch
+  it_behaves_like 'ee protected ref access', :protected_branch
+  it_behaves_like 'protected branch access'
+  it_behaves_like 'protected ref access allowed_access_levels', excludes: [Gitlab::Access::NO_ACCESS]
+  it_behaves_like 'protected ref access configured for users', :protected_branch
+  it_behaves_like 'protected ref access configured for groups', :protected_branch
 end
