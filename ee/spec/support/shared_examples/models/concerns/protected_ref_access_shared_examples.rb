849784e42d78d25aa83e9c83b787c98178cad254
Joe Woodward <j@joewoodward.me>
2024-06-17 18:14:32 +0100

    Implement AR validations for ProtectedRefAccess
    Prior to this change we did not validate that the user or group actually
    existed when creating a ProtectedRefAccess object.
    
    We have foreign_key constraints but this means we relied on the database
    layer to raise exceptions instead of returning user friendly errors.
    
    Also refactors how the validations are structured so it's simpler to see
    what validations are ignored during importing.
    
    Related to https://gitlab.com/gitlab-org/gitlab/-/issues/461213
    
    Changelog: changed
    EE: true

--- ee/spec/support/shared_examples/models/concerns/protected_ref_access_shared_examples.rb
+++ ee/spec/support/shared_examples/models/concerns/protected_ref_access_shared_examples.rb
@@ -14,6 +14,233 @@ RSpec.shared_examples 'ee protected ref access' do |association|
     create(:project_group_link, group: group, project: project)
   end
 
+  describe 'Validations:' do
+    let(:access_user_id) { nil }
+    let(:access_group_id) { nil }
+    let(:access_user) { nil }
+    let(:access_group) { nil }
+    let(:importing) { false }
+
+    subject do
+      build(
+        described_class.model_name.singular.to_sym,
+        association => protected_ref,
+        user_id: access_user_id,
+        group_id: access_group_id,
+        importing: importing
+      ).tap do |instance|
+        # We need to assign manually after building because AR sets the
+        # association to nil if the fk attributes are passed including nil.
+        instance.user = access_user if access_user
+        instance.group = access_group if access_group
+      end
+    end
+
+    shared_context 'when feature :protected_refs_for_users is enabled' do
+      before do
+        allow(project).to receive(:feature_available?).with(:protected_refs_for_users).and_return(true)
+      end
+    end
+
+    shared_context 'when feature :protected_refs_for_users is disabled' do
+      before do
+        allow(project).to receive(:feature_available?).with(:protected_refs_for_users).and_return(false)
+      end
+    end
+
+    shared_context 'and not a role based access level' do
+      before do
+        allow(subject).to receive(:role?).and_return(false)
+      end
+    end
+
+    shared_context 'and is a role based access level' do
+      before do
+        allow(subject).to receive(:role?).and_return(true)
+      end
+    end
+
+    shared_examples 'validates user_id and group_id absence' do
+      it { is_expected.to validate_absence_of(:group_id) }
+      it { is_expected.to validate_absence_of(:user_id) }
+    end
+
+    shared_examples 'does not validate user_id and group_id absence' do
+      it { is_expected.not_to validate_absence_of(:group_id) }
+      it { is_expected.not_to validate_absence_of(:user_id) }
+    end
+
+    shared_examples 'validates user and group exist' do
+      context 'and group_id is present' do
+        let(:access_group_id) { 0 }
+
+        it do
+          is_expected.not_to be_valid
+          expect(subject.errors.where(:group, :blank)).to be_present
+        end
+      end
+
+      context 'and user_id is present' do
+        let(:access_user_id) { 0 }
+
+        it do
+          is_expected.not_to be_valid
+          expect(subject.errors.where(:user, :blank)).to be_present
+        end
+      end
+    end
+
+    shared_examples 'does not validate user and group exist' do
+      context 'and group_id is present' do
+        let(:access_group_id) { group.id }
+
+        it { is_expected.not_to validate_presence_of(:group) }
+      end
+
+      context 'and user_id is present' do
+        let(:access_user_id) { user.id }
+
+        it { is_expected.not_to validate_presence_of(:user) }
+      end
+    end
+
+    shared_examples 'validates user and group membership' do
+      context 'and group is present' do
+        let(:access_group) { group }
+
+        before do
+          allow(subject).to receive(:validate_group_membership)
+          subject.valid?
+        end
+
+        it { is_expected.to have_received(:validate_group_membership) }
+      end
+
+      context 'and user is present' do
+        let(:access_user) { user }
+
+        before do
+          allow(subject).to receive(:validate_user_membership)
+          subject.valid?
+        end
+
+        it { is_expected.to have_received(:validate_user_membership) }
+      end
+    end
+
+    shared_examples 'does not validate user and group membership' do
+      context 'and group is present' do
+        let(:access_group) { group }
+
+        before do
+          allow(subject).to receive(:validate_group_membership)
+          subject.valid?
+        end
+
+        it { is_expected.not_to have_received(:validate_group_membership) }
+      end
+
+      context 'and user is present' do
+        let(:access_user) { user }
+
+        before do
+          allow(subject).to receive(:validate_user_membership)
+          subject.valid?
+        end
+
+        it { is_expected.not_to have_received(:validate_user_membership) }
+      end
+    end
+
+    context 'when not importing' do
+      let(:importing) { false }
+
+      context 'when feature :protected_refs_for_users is enabled' do
+        include_context 'when feature :protected_refs_for_users is enabled'
+
+        context 'and not a role based access level' do
+          include_context 'and not a role based access level'
+
+          it_behaves_like 'does not validate user_id and group_id absence'
+          it_behaves_like 'validates user and group exist'
+          it_behaves_like 'validates user and group membership'
+        end
+
+        context 'and is a role based access level' do
+          include_context 'and is a role based access level'
+
+          it_behaves_like 'validates user_id and group_id absence'
+          it_behaves_like 'does not validate user and group exist'
+          it_behaves_like 'does not validate user and group membership'
+        end
+      end
+
+      context 'when feature :protected_refs_for_users is disabled' do
+        include_context 'when feature :protected_refs_for_users is disabled'
+
+        context 'and not a role based access level' do
+          include_context 'and not a role based access level'
+
+          it_behaves_like 'validates user_id and group_id absence'
+          it_behaves_like 'does not validate user and group exist'
+          it_behaves_like 'does not validate user and group membership'
+        end
+
+        context 'and is a role based access level' do
+          include_context 'and is a role based access level'
+
+          it_behaves_like 'validates user_id and group_id absence'
+          it_behaves_like 'does not validate user and group exist'
+          it_behaves_like 'does not validate user and group membership'
+        end
+      end
+    end
+
+    context 'when importing' do
+      let(:importing) { true }
+
+      context 'when feature :protected_refs_for_users is enabled' do
+        include_context 'when feature :protected_refs_for_users is enabled'
+
+        context 'and not a role based access level' do
+          include_context 'and not a role based access level'
+
+          it_behaves_like 'does not validate user_id and group_id absence'
+          it_behaves_like 'does not validate user and group exist'
+          it_behaves_like 'does not validate user and group membership'
+        end
+
+        context 'and is a role based access level' do
+          include_context 'and is a role based access level'
+
+          it_behaves_like 'does not validate user_id and group_id absence'
+          it_behaves_like 'does not validate user and group exist'
+          it_behaves_like 'does not validate user and group membership'
+        end
+      end
+
+      context 'when feature :protected_refs_for_users is disabled' do
+        include_context 'when feature :protected_refs_for_users is disabled'
+
+        context 'and not a role based access level' do
+          include_context 'and not a role based access level'
+
+          it_behaves_like 'does not validate user_id and group_id absence'
+          it_behaves_like 'does not validate user and group exist'
+          it_behaves_like 'does not validate user and group membership'
+        end
+
+        context 'and is a role based access level' do
+          include_context 'and is a role based access level'
+
+          it_behaves_like 'does not validate user_id and group_id absence'
+          it_behaves_like 'does not validate user and group exist'
+          it_behaves_like 'does not validate user and group membership'
+        end
+      end
+    end
+  end
+
   describe '#type' do
     using RSpec::Parameterized::TableSyntax
 
