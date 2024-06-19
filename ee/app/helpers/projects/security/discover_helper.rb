e9eea5f2cff7648915c6bdec58af9ceae9a56e00
rliu-gl <rliu@gitlab.com>
2024-06-19 13:45:13 -0400

    Remove showcase_free_security_features experiment
    Changelog: changed
    EE: true

--- ee/app/helpers/projects/security/discover_helper.rb
+++ ee/app/helpers/projects/security/discover_helper.rb
@@ -1,19 +1,6 @@
 # frozen_string_literal: true
 
 module Projects::Security::DiscoverHelper
-  def project_security_showcase_data(project)
-    {
-      billing_vulnerability_management: group_billings_path(project.root_ancestor, glm_content: 'security-showcase-vulnerability-management', glm_source: 'gitlab.com'),
-      billing_dependency_scanning: group_billings_path(project.root_ancestor, glm_content: 'security-showcase-dependency-scanning', glm_source: 'gitlab.com'),
-      billing_dast: group_billings_path(project.root_ancestor, glm_content: 'security-showcase-dast', glm_source: 'gitlab.com'),
-      billing_container_scanning: group_billings_path(project.root_ancestor, glm_content: 'security-showcase-container-scanning', glm_source: 'gitlab.com'),
-      trial_vulnerability_management: new_trial_registration_path(project.root_ancestor, glm_content: 'security-showcase-vulnerability-management', glm_source: 'gitlab.com'),
-      trial_dependency_scanning: new_trial_registration_path(project.root_ancestor, glm_content: 'security-showcase-dependency-scanning', glm_source: 'gitlab.com'),
-      trial_dast: new_trial_registration_path(project.root_ancestor, glm_content: 'security-showcase-dast', glm_source: 'gitlab.com'),
-      trial_container_scanning: new_trial_registration_path(project.root_ancestor, glm_content: 'security-showcase-container-scanning', glm_source: 'gitlab.com')
-    }
-  end
-
   def project_security_discover_data(project)
     content = 'discover-project-security'
     link_upgrade = project.personal? ? profile_billings_path(project.group, source: content) : group_billings_path(project.root_ancestor, source: content)
