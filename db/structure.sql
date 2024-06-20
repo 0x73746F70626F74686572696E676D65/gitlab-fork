76a7a8d7550146eb4b9f587e91e13399ba55b98e
Heinrich Lee Yu <heinrich@gitlab.com>
2024-06-20 23:33:27 +0800

    Enforce new Markdown upload URL format
    Enforce the use of ID-based URLs for Markdown uploads. Only legacy
    uploads will be available on the path-based URLs.

--- db/structure.sql
+++ db/structure.sql
@@ -18035,6 +18035,7 @@ CREATE TABLE uploads (
     store integer DEFAULT 1,
     mount_point character varying,
     secret character varying,
+    version integer DEFAULT 1 NOT NULL,
     CONSTRAINT check_5e9547379c CHECK ((store IS NOT NULL))
 );
 
