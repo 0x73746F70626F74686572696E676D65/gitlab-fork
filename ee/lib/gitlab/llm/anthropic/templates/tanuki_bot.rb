be4489f827664fb4fadba98b8e0a5b87ec2b317d
Mark Chao <mchao@gitlab.com>
2024-06-20 11:15:46 +0800

    Update prompt to not include ATTRS part
    if doc can't answer the question.

--- ee/lib/gitlab/llm/anthropic/templates/tanuki_bot.rb
+++ ee/lib/gitlab/llm/anthropic/templates/tanuki_bot.rb
@@ -12,6 +12,22 @@ module Gitlab
 
           MAIN_PROMPT = <<~PROMPT
             Given the following extracted parts of technical documentation enclosed in <quote></quote> XML tags and a question, create a final answer.
+
+            If you know the answer:
+              1. answer it
+              2. at the end return a %<content_id>s part for references (ALWAYS name it %<content_id>s).
+
+            If you don't know the answer:
+              1. just say "I don't know based on the documentation", and don't try to make up an answer
+              2. Do not add %<content_id>s part.
+
+            QUESTION: %<question>s
+
+            %<content>s
+          PROMPT
+
+          OLD_MAIN_PROMPT = <<~PROMPT
+            Given the following extracted parts of technical documentation enclosed in <quote></quote> XML tags and a question, create a final answer.
             If you don't know the answer, just say that you don't know. Don't try to make up an answer.
             At the end of your answer ALWAYS return a "%<content_id>s" part for references and
             ALWAYS name it %<content_id>s.
@@ -62,7 +78,7 @@ module Gitlab
 
           def self.main_prompt(question:, content:)
             format(
-              MAIN_PROMPT,
+              Feature.enabled?(:ai_doc_tool_hide_reference) ? MAIN_PROMPT : OLD_MAIN_PROMPT, # rubocop:disable Gitlab/FeatureFlagWithoutActor -- reduce code complexity
               question: question,
               content: content,
               content_id: CONTENT_ID_FIELD
