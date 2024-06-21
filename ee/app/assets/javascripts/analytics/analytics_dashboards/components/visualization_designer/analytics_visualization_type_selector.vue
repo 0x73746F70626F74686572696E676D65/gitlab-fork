f7e5cdd2aa4d7eabb99d4490976844d6a111e369
Elwyn Benson <ebenson@gitlab.com>
2024-06-21 14:49:21 +1200

    Update visualization type selection with validation state
    Replaces the product analytics visualization designer "type" dropdown
    component with a GlFormSelect. This enables an invalid state to be added
    to this element when form validation is triggered.
    
    Changelog: changed
    EE: true

--- ee/app/assets/javascripts/analytics/analytics_dashboards/components/visualization_designer/analytics_visualization_type_selector.vue
+++ ee/app/assets/javascripts/analytics/analytics_dashboards/components/visualization_designer/analytics_visualization_type_selector.vue
@@ -1,26 +1,27 @@
 <script>
-import { GlDropdown, GlDropdownItem } from '@gitlab/ui';
+import { GlFormSelect } from '@gitlab/ui';
 import { s__ } from '~/locale';
 
 const TYPES = [
   {
+    value: '',
+    disabled: true,
+    text: s__('Analytics|Select a visualization type'),
+  },
+  {
     value: 'LineChart',
-    icon: 'chart',
     text: s__('Analytics|Line chart'),
   },
   {
     value: 'ColumnChart',
-    icon: 'chart',
     text: s__('Analytics|Column chart'),
   },
   {
     value: 'DataTable',
-    icon: 'table',
     text: s__('Analytics|Data table'),
   },
   {
     value: 'SingleStat',
-    icon: 'table',
     text: s__('Analytics|Single statistic'),
   },
 ];
@@ -28,29 +29,17 @@ const TYPES = [
 export default {
   name: 'AnalyticsVisualizationTypeSelector',
   components: {
-    GlDropdown,
-    GlDropdownItem,
+    GlFormSelect,
   },
   props: {
-    selectedVisualizationType: {
+    state: {
+      type: Boolean,
+      required: false,
+      default: null,
+    },
+    value: {
       type: String,
       required: true,
-    },
-  },
-  computed: {
-    selectedType() {
-      return TYPES.find((type) => type.value === this.selectedVisualizationType);
-    },
-    selectedText() {
-      return this.selectedType?.text || s__('Analytics|Select a visualization type');
-    },
-    selectedIcon() {
-      return this.selectedType?.icon;
-    },
-  },
-  methods: {
-    selectVisualizationType(visualizationType) {
-      this.$emit('selectVisualizationType', visualizationType);
     },
   },
   TYPES,
@@ -58,14 +47,10 @@ export default {
 </script>
 
 <template>
-  <gl-dropdown :text="selectedText" :icon="selectedIcon" block>
-    <gl-dropdown-item
-      v-for="type in $options.TYPES"
-      :key="type.value"
-      :icon-name="type.icon"
-      @click="selectVisualizationType(type.value)"
-    >
-      {{ type.text }}
-    </gl-dropdown-item>
-  </gl-dropdown>
+  <gl-form-select
+    :value="value"
+    :state="state"
+    :options="$options.TYPES"
+    @input="$emit('input', $event)"
+  />
 </template>
