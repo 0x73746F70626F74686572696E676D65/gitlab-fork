196a6bd16e74451b8d9d26cd8e5185110f30bf5d
Mark Chao <mchao@gitlab.com>
2024-06-24 12:01:28 +0000

    Remove ai_embedding_fixture RSpec metadata
    Remove reference to VERTEX_AI_PROJECT as it is not used anymore

--- ee/spec/support/llm.rb
+++ ee/spec/support/llm.rb
@@ -4,9 +4,9 @@ RSpec.configure do |config|
   config.around(:each, :real_ai_request) do |example|
     real_ai_request_bool = ActiveModel::Type::Boolean.new.cast(ENV['REAL_AI_REQUEST'])
 
-    if !real_ai_request_bool || !ENV['ANTHROPIC_API_KEY'] || !ENV['VERTEX_AI_PROJECT']
+    if !real_ai_request_bool || !ENV['ANTHROPIC_API_KEY']
       puts "skipping '#{example.description}' because it does real third-party requests, set " \
-           "REAL_AI_REQUEST=true, ANTHROPIC_API_KEY='<key>' and VERTEX_AI_PROJECT=<project-id>"
+           "REAL_AI_REQUEST=true, ANTHROPIC_API_KEY='<key>'"
       next
     end
 
@@ -18,21 +18,5 @@ RSpec.configure do |config|
   config.before(:each, :real_ai_request) do
     allow(Gitlab::CurrentSettings.current_application_settings).to receive(:anthropic_api_key)
       .at_least(:once).and_return(ENV['ANTHROPIC_API_KEY'])
-    allow(Gitlab::CurrentSettings.current_application_settings).to receive(:vertex_ai_credentials)
-      .at_least(:once).and_return(ENV['VERTEX_AI_CREDENTIALS'])
-    allow(Gitlab::CurrentSettings.current_application_settings).to receive(:vertex_ai_project)
-      .at_least(:once).and_return(ENV['VERTEX_AI_PROJECT'])
-  end
-
-  config.before(:context, :ai_embedding_fixtures) do
-    add_vertex_embeddings_from_fixture unless ::Embedding::Vertex::GitlabDocumentation.any?
-  end
-
-  def add_vertex_embeddings_from_fixture
-    fixture_path = Rails.root.join("ee/spec/fixtures/vertex_embeddings")
-    embedding_model = ::Embedding::Vertex::GitlabDocumentation
-    copy_from_statement = "COPY #{embedding_model.table_name} FROM '#{fixture_path}'"
-
-    embedding_model.connection.execute(copy_from_statement)
   end
 end
