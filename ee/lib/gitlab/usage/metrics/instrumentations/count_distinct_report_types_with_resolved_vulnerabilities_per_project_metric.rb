50ff627ba8ce0309e9acfaec3283575e842e5a77
Lucas Charles <me@lucascharles.me>
2024-06-24 17:02:21 +0000

    Apply 2 suggestion(s) to 1 file(s)
    Co-authored-by: Jonas Larsen <jlarsen@gitlab.com>

---
+++ ee/lib/gitlab/usage/metrics/instrumentations/count_distinct_report_types_with_resolved_vulnerabilities_per_project_metric.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module Usage
+    module Metrics
+      module Instrumentations
+        class CountDistinctReportTypesWithResolvedVulnerabilitiesPerProjectMetric < DatabaseMetric
+          operation :count
+
+          timestamp_column('vulnerability_state_transitions.created_at')
+
+          # We must override value since we are not able to batch this query due to usage of the count subquery
+          def value
+            relation.count
+          end
+
+          def relation
+            base_relation = Vulnerabilities::Read.joins(
+              <<-SQL
+                INNER JOIN vulnerability_state_transitions
+                  ON vulnerability_state_transitions.vulnerability_id = vulnerability_reads.vulnerability_id
+              SQL
+            ).where(
+              vulnerability_state_transitions: { to_state: Enums::Vulnerability::VULNERABILITY_STATES[:resolved] }
+            ).where(time_constraints).group(
+              :project_id,
+              :report_type
+            ).distinct.select(
+              :project_id,
+              :report_type
+            )
+
+            Vulnerabilities::Read.from(base_relation)
+          end
+        end
+      end
+    end
+  end
+end
