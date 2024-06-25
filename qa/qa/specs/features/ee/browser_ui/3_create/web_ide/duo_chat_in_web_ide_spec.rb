d23fdbe4379dc3532a7339d70d342fadf368a977
Jay McCure <jmccure@gitlab.com>
2024-06-25 16:19:56 +1000

    E2E test: add SM duo chat scenario web ide

--- qa/qa/specs/features/ee/browser_ui/3_create/web_ide/duo_chat_in_web_ide_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/3_create/web_ide/duo_chat_in_web_ide_spec.rb
@@ -2,8 +2,26 @@
 
 module QA
   # https://docs.gitlab.com/ee/development/ai_features/duo_chat.html
-  RSpec.describe 'Create', :external_ai_provider, product_group: :ide do
+  RSpec.describe 'Create', product_group: :ide do
     describe 'Duo Chat in Web IDE' do
+      shared_examples 'Duo Chat' do |testcase|
+        it 'gets a response back from Duo Chat', testcase: testcase do
+          Page::Project::WebIDE::VSCode.perform do |ide|
+            ide.open_duo_chat
+            ide.within_vscode_duo_chat do
+              QA::EE::Page::Component::DuoChat.perform do |duo_chat|
+                duo_chat.clear_chat_history
+                expect(duo_chat).to be_empty_state
+
+                duo_chat.send_duo_chat_prompt('hi')
+                expect(duo_chat.has_response?(expected_response)).to be_truthy,
+                  "Expected '#{expected_response}' within Duo Chat response."
+              end
+            end
+          end
+        end
+      end
+
       let(:project) { create(:project, :with_readme, name: 'webide-duo-chat-project') }
 
       before do
@@ -13,22 +31,19 @@ module QA
         Page::Project::WebIDE::VSCode.perform(&:wait_for_ide_to_load)
       end
 
-      context 'when initiating Duo Chat' do
-        it 'returns a response to a simple request', only: { pipeline: %i[staging staging-canary canary production] },
-          testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/443762' do
-          Page::Project::WebIDE::VSCode.perform do |ide|
-            ide.open_duo_chat
-            ide.within_vscode_duo_chat do
-              QA::EE::Page::Component::DuoChat.perform do |duo_chat|
-                duo_chat.clear_chat_history
-                expect(duo_chat).to be_empty_state
+      context 'on GitLab.com', :external_ai_provider,
+        only: { pipeline: %i[staging staging-canary canary production] } do
+        let(:expected_response) { 'GitLab' }
 
-                duo_chat.send_duo_chat_prompt('hi')
-                expect(duo_chat.has_response?('GitLab')).to be_truthy, 'Expected "GitLab" within Duo Chat response.'
-              end
-            end
-          end
-        end
+        it_behaves_like 'Duo Chat', 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/443762'
+      end
+
+      context 'on Self-managed', :orchestrated, :ai_gateway do
+        # As an orchestrated test we use an ai-gateway with a fake model, so we can assert part of the prompt
+        # https://gitlab.com/gitlab-org/gitlab/-/blob/481a3af0ded95cb24fc1e34b004d104c72ed95e4/ee/lib/gitlab/llm/chain/agents/zero_shot/executor.rb#L229-229
+        let(:expected_response) { 'Question: the input question you must answer' }
+
+        it_behaves_like 'Duo Chat', 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/468854'
       end
     end
   end
