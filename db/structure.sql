a60fc3e7de885090d52a283f1ea2358761942c85
Oscar Tovar <otovar@gitlab.com>
2024-06-26 15:12:43 -0400

    Remove "sbom_components"."source_package_name" col
    This column as ignored in 17.1, and is now ready for removal in 17.2.
    For context, this column was never used since an implementation change
    was made so that we used "sbom_component_versions"."source_package_name"
    instead.

--- db/structure.sql
+++ db/structure.sql
@@ -12314,7 +12314,7 @@ CREATE TABLE merge_request_context_commits (
     message text,
     merge_request_id bigint,
     trailers jsonb DEFAULT '{}'::jsonb NOT NULL,
-    project_id bigint
+    project_id bigint,
     CONSTRAINT check_1dc6b5f2ac CHECK ((merge_request_id IS NOT NULL))
 );
 
@@ -16799,9 +16799,7 @@ CREATE TABLE sbom_components (
     component_type smallint NOT NULL,
     name text NOT NULL,
     purl_type smallint,
-    source_package_name text,
-    CONSTRAINT check_91a8f6ad53 CHECK ((char_length(name) <= 255)),
-    CONSTRAINT check_e2dcb53709 CHECK ((char_length(source_package_name) <= 255))
+    CONSTRAINT check_91a8f6ad53 CHECK ((char_length(name) <= 255))
 );
 
 CREATE SEQUENCE sbom_components_id_seq
