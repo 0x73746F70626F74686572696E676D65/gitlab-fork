bc90a50bc57f11bc252cbc57e7ff4f887d5571c5
James Nutt <jnutt@gitlab.com>
2024-07-02 11:06:53 +0100

    Skip approval updates for merged MRs
    This callback is causing note imports to fail for projects with aproval
    rules configured. When a merge request is imported, if
    `approvals_before_merge` is not nil and the MR has already been merged,
    the approval rule generated by `any_approver_rule` will fail validation,
    and the subsequent call to `update_column` will result in an exception.
    
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/138154
    validates against updating approval rules for merged MRs, so in this
    case it should be safe to skip the callback for merged MRs.
    
    Changelog: fixed
    EE: true

--- ee/spec/models/concerns/deprecated_approvals_before_merge_spec.rb
+++ ee/spec/models/concerns/deprecated_approvals_before_merge_spec.rb
@@ -2,9 +2,9 @@
 
 require 'spec_helper'
 
-RSpec.describe DeprecatedApprovalsBeforeMerge do
+RSpec.describe DeprecatedApprovalsBeforeMerge, feature_category: :source_code_management do
   shared_examples 'with approvals before merge deprecated' do
-    context 'updating approvals_before_merge' do
+    context 'when updating approvals_before_merge' do
       it 'creates any_approver rule' do
         subject.update!(approvals_before_merge: 3)
 
@@ -17,13 +17,23 @@ RSpec.describe DeprecatedApprovalsBeforeMerge do
     end
   end
 
-  context 'merge request' do
+  context 'for unmerged merge request' do
     subject { create(:merge_request, approvals_before_merge: 2) }
 
     it_behaves_like 'with approvals before merge deprecated'
   end
 
-  context 'project' do
+  context 'for merged MR' do
+    subject(:merge_request) { create(:merge_request, :merged, approvals_before_merge: 2) }
+
+    it 'does not change approval rules' do
+      expect(merge_request).not_to receive(:update_any_approver_rule)
+
+      merge_request.update!(approvals_before_merge: 3)
+    end
+  end
+
+  context 'for project' do
     subject { create(:project, approvals_before_merge: 2) }
 
     it_behaves_like 'with approvals before merge deprecated'
