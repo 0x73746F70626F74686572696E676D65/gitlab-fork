60ebf9a755142a010bd47eb5e6e0e7f16a7d3122
Matthias Käppler <mkaeppler@gitlab.com>
2024-07-02 19:31:58 +0000

    Update Rake task for AI dev setup
    This adds several useful tasks to help with AI
    related development setup, such as creating
    fake add-on purchase records.

--- ee/lib/gitlab/duo/developments/setup.rb
+++ ee/lib/gitlab/duo/developments/setup.rb
@@ -4,88 +4,67 @@
 module Gitlab
   module Duo
     module Developments
-      class Setup
-        attr_reader :args
+      class SelfManagedStrategy
+        def execute
+          puts <<~TXT.strip
+          ================================================================================
+          ## Running self-managed mode setup
+          ## If you want to run .com mode, set GITLAB_SIMULATE_SAAS=1
+          ================================================================================
+          TXT
 
-        def initialize(args)
-          @args = args
+          require_self_managed!
+        end
+
+        private
+
+        # rubocop:disable Style/GuardClause -- For reading simplicity
+        def require_self_managed!
+          if ::Gitlab::Utils.to_boolean(ENV['GITLAB_SIMULATE_SAAS'])
+            raise <<~MSG
+              Make sure 'GITLAB_SIMULATE_SAAS' environment variable is false or not set.
+              See https://docs.gitlab.com/ee/development/ee_features.html#simulate-a-saas-instance for more information.
+            MSG
+          end
+        end
+        # rubocop:enable Style/GuardClause
+      end
+
+      class GitlabComStrategy
+        def initialize(namespace)
+          @namespace = namespace
         end
 
         def execute
-          validates!
+          puts <<~TXT.strip
+          ================================================================================
+          ## Running .com mode setup for group '#{@namespace}'
+          ================================================================================
+          TXT
 
-          ensure_feature_flags
-          ensure_application_settings
+          require_dot_com!
+          ensure_application_settings!
+
           group = ensure_group
-          ensure_license_activated(group)
-          ensure_group_settings(group)
-
-          print_output(group)
+          ensure_group_subscription!(group)
+          ensure_group_settings!(group)
         end
 
-        # rubocop:disable Style/GuardClause -- Keep it explicit
-        def validates!
-          puts "Validating settings...."
+        private
 
-          unless ::Gitlab.dev_or_test_env?
-            raise <<~MSG
-              Setup can only be performed in development or test environment, however, the current environment is #{ENV['RAILS_ENV']}.
-            MSG
-          end
-
+        def require_dot_com!
+          # rubocop:disable Style/GuardClause -- For reading simplicity
           unless ::Gitlab::Utils.to_boolean(ENV['GITLAB_SIMULATE_SAAS'])
             raise <<~MSG
               Make sure 'GITLAB_SIMULATE_SAAS' environment variable is truthy.
               See https://docs.gitlab.com/ee/development/ee_features.html#simulate-a-saas-instance for more information.
             MSG
           end
-        end
-        # rubocop:enable Style/GuardClause
-
-        def ensure_feature_flags
-          puts "Enabling feature flags...."
-
-          ::Feature.enable(:summarize_my_code_review)
-        end
-
-        def ensure_application_settings
-          puts "Enabling application settings...."
-
-          Gitlab::CurrentSettings.current_application_settings
-            .update!(check_namespace_plan: true, allow_local_requests_from_web_hooks_and_services: true)
-        end
-
-        def ensure_group
-          puts "Checking the specified group exists...."
-
-          raise "You must specify :root_group_path" unless args[:root_group_path].present?
-          raise "Sub group cannot be specified" if args[:root_group_path].include?('/')
-
-          group = Group.find_by_full_path(args[:root_group_path])
-
-          if group
-            puts "Found the group: #{group.name}"
-            return group
-          end
-
-          puts "The specified group is not found. Creating a new one..."
-
-          current_user = User.first
-          group_params = {
-            name: args[:root_group_path],
-            path: args[:root_group_path],
-            visibility_level: ::Featurable::ENABLED
-          }
-          response = Groups::CreateService.new(current_user, group_params).execute
-          group = response[:group]
-
-          raise "Failed to create a group: #{group.errors.full_messages}" if response.error?
-
-          group
+          # rubocop:enable Style/GuardClause
         end
 
         # rubocop:disable CodeReuse/ActiveRecord -- Development purpose
-        def ensure_license_activated(group)
+        def ensure_group_subscription!(group)
           puts "Activating an Ultimate license to the group...."
 
           plan = Plan.find_or_create_by(name: "ultimate", title: "Ultimate")
@@ -96,23 +75,135 @@ module Gitlab
         end
         # rubocop:enable CodeReuse/ActiveRecord
 
-        def ensure_group_settings(group)
+        def ensure_application_settings!
+          puts "Enabling application settings...."
+
+          Gitlab::CurrentSettings.current_application_settings
+            .update!(check_namespace_plan: true, allow_local_requests_from_web_hooks_and_services: true)
+        end
+
+        def ensure_group
+          puts "Checking the specified group exists...."
+
+          raise "You must specify :root_group_path" unless @namespace.present?
+          raise "Provided group name must be a root group" if @namespace.include?('/')
+
+          group = Group.find_by_full_path(@namespace)
+
+          if group
+            puts "Found the group: #{group.name}"
+
+            return group
+          end
+
+          puts "The specified group is not found. Creating a new one..."
+
+          current_user = User.first
+          org = create_org(current_user)
+          group_params = {
+            name: @namespace,
+            path: @namespace,
+            organization: org,
+            visibility_level: ::Featurable::ENABLED
+          }
+          response = Groups::CreateService.new(current_user, group_params).execute
+          group = response[:group]
+
+          raise "Failed to create a group: #{group.errors.full_messages}" if response.error?
+
+          group
+        end
+
+        def create_org(current_user)
+          response = ::Organizations::CreateService.new(
+            current_user: current_user,
+            params: { name: @namespace, path: @namespace }
+          ).execute
+          org = response[:organization]
+
+          raise "Failed to create an org: #{response.errors}" if response.error?
+
+          org
+        end
+
+        def ensure_group_settings!(group)
           puts "Enabling the group settings...."
 
           group = Group.find(group.id) # Hard Reload for refreshing the cache
           group.update!(experiment_features_enabled: true)
         end
+      end
 
-        def print_output(group)
+      class Setup
+        attr_reader :args
+
+        def initialize(args)
+          @args = args
+          @namespace = args[:root_group_path]
+          @setup_strategy = @namespace.present? ? GitlabComStrategy.new(@namespace) : SelfManagedStrategy.new
+        end
+
+        def execute
+          ensure_dev_mode!
+          ensure_feature_flags!
+          ensure_license!
+          @setup_strategy.execute
+          create_add_on_purchase!
+
+          print_result
+        end
+
+        private
+
+        # rubocop:disable Style/GuardClause -- Keep it explicit
+        def ensure_dev_mode!
+          unless ::Gitlab.dev_or_test_env?
+            raise <<~MSG
+              Setup can only be performed in development or test environment, however, the current environment is #{ENV['RAILS_ENV']}.
+            MSG
+          end
+        end
+        # rubocop:enable Style/GuardClause
+
+        def ensure_feature_flags!
+          puts "Enabling feature flags...."
+
+          ::Feature.enable(:summarize_my_code_review)
+          ::Feature.enable(:self_managed_code_suggestions)
+          ::Feature.enable(:gitlab_com_duo_pro_bulk_user_assignment)
+          ::Feature.enable(:enable_hamilton_in_user_preferences)
+          ::Feature.enable(:code_suggestions_direct_completions)
+          ::Feature.enable(:allow_organization_creation)
+        end
+
+        def ensure_license!
+          license = ::License.current
+          raise 'No license found' unless license
+        end
+
+        def create_add_on_purchase!
+          add_on = ::GitlabSubscriptions::AddOn.find_or_create_by_name(:code_suggestions)
+          group = Group.find_by_full_path(@namespace) # will be nil for self-managed mode
+
+          # rubocop: disable Cop/DestroyAll -- For dev
+          ::GitlabSubscriptions::AddOnPurchase.destroy_all
+          # rubocop: enable Cop/DestroyAll
+          resp = ::GitlabSubscriptions::AddOnPurchases::CreateService.new(group, add_on, {
+            quantity: 100,
+            expires_on: 1.year.from_now,
+            purchase_xid: 'C-12345'
+          }).execute
+
+          raise resp.message unless resp.success?
+        end
+
+        def print_result
           puts <<~MSG
             ----------------------------------------
             Setup Complete!
             ----------------------------------------
 
-            Visit "#{Gitlab.config.gitlab.protocol}://#{Gitlab.config.gitlab.host}:#{Gitlab.config.gitlab.port}/#{group.full_path}" for testing GitLab Duo features.
-
-            All GitLab Duo features except Code Suggestions should be available under the root group.
-            To setup Code Suggestions, see https://docs.gitlab.com/ee/development/code_suggestions/.
+            Visit "#{Gitlab.config.gitlab.protocol}://#{Gitlab.config.gitlab.host}:#{Gitlab.config.gitlab.port}/#{@namespace.presence}" for testing GitLab Duo features.
 
             For more development guidelines, see https://docs.gitlab.com/ee/development/ai_features/index.html.
           MSG
--- ee/spec/lib/gitlab/duo/developments/setup_spec.rb
+++ ee/spec/lib/gitlab/duo/developments/setup_spec.rb
@@ -2,40 +2,40 @@
 
 require 'spec_helper'
 
-RSpec.describe Gitlab::Duo::Developments::Setup, :saas, :gitlab_duo, :silence_stdout, feature_category: :duo_chat do
+RSpec.describe Gitlab::Duo::Developments::Setup, :gitlab_duo, :silence_stdout, feature_category: :duo_chat do
   include RakeHelpers
 
-  let_it_be(:group) { create(:group, path: 'test-group') }
+  let_it_be(:group) { create(:group, :with_organization, path: 'test-group') }
   let_it_be(:project) { create(:project, group: group) }
   let_it_be(:user) { create(:user, maintainer_of: project) }
 
   let(:task) { described_class.new(args) }
-  let(:args) { { root_group_path: group.path } }
   let(:rake_task) { instance_double(Rake::Task, invoke: true) }
+
+  let(:feature_flags) do
+    [
+      :summarize_my_code_review,
+      :self_managed_code_suggestions,
+      :gitlab_com_duo_pro_bulk_user_assignment,
+      :enable_hamilton_in_user_preferences,
+      :code_suggestions_direct_completions,
+      :allow_organization_creation
+    ]
+  end
 
   subject(:setup) { task.execute }
 
   before do
     allow(Rake::Task).to receive(:[]).with(any_args).and_return(rake_task)
-    allow(::Gitlab::CurrentSettings).to receive(:anthropic_api_key).and_return('a-fake-key')
-    allow_next_instance_of(::Gitlab::Llm::VertexAi::TokenLoader) do |token_loader|
-      allow(token_loader).to receive(:current_token).and_return('a-fake-token')
-    end
-
-    stub_env('GITLAB_SIMULATE_SAAS', '1')
-
+    feature_flags.each { |flag| ::Feature.disable(flag) }
     create_current_license_without_expiration(plan: License::ULTIMATE_PLAN)
   end
 
-  context 'when group doest not exist' do
-    let(:args) { { root_group_path: 'new-path' } }
-
-    it 'creates a new group' do
-      expect { setup }.to change { ::Group.count }.by(1)
-    end
-
-    context 'when failed to create a group' do
-      let(:args) { { root_group_path: '!!!!!' } }
+  shared_examples 'checks for dev or test env' do
+    context 'with production environment' do
+      before do
+        allow(::Gitlab).to receive(:dev_or_test_env?).and_return(false)
+      end
 
       it 'raises an error' do
         expect { setup }.to raise_error(RuntimeError)
@@ -43,39 +43,103 @@ RSpec.describe Gitlab::Duo::Developments::Setup, :saas, :gitlab_duo, :silence_st
     end
   end
 
-  context 'when group already exists' do
-    it 'does not create a new group' do
-      expect { setup }.not_to change { ::Group.count }
+  shared_examples 'errors when GITLAB_SIMULATE_SAAS has unexpected value' do |expected_gitlab_simulate_saas_env|
+    context 'when GITLAB_SIMULATE_SAAS has unexpected value' do
+      before do
+        stub_const('ENV', { 'GITLAB_SIMULATE_SAAS' => !expected_gitlab_simulate_saas_env })
+      end
+
+      it 'raises an error' do
+        expect { setup }.to raise_error(RuntimeError)
+      end
     end
   end
 
-  context 'with production environment' do
+  shared_examples 'enables all necessary feature flags' do
+    it 'enables all necessary feature flags', :aggregate_failures do
+      setup
+
+      feature_flags.each do |flag|
+        expect(::Feature.enabled?(flag)).to be_truthy # rubocop:disable Gitlab/FeatureFlagWithoutActor -- For dev
+      end
+    end
+  end
+
+  shared_examples 'errors when there is no license' do
+    context 'when there is no license' do
+      it 'raises an error' do
+        License.delete_all
+
+        expect { setup }.to raise_error(RuntimeError)
+      end
+    end
+  end
+
+  context 'when simulating GitLabCom: passing target group as an argument', :saas do
+    let(:args) { { root_group_path: group.path } }
+
     before do
-      allow(::Gitlab).to receive(:dev_or_test_env?).and_return(false)
+      stub_env('GITLAB_SIMULATE_SAAS', '1')
     end
 
-    it 'raises an error' do
-      expect { setup }.to raise_error(RuntimeError)
+    context 'when group does not exist' do
+      let(:args) { { root_group_path: 'new-path' } }
+
+      it 'creates a new group' do
+        expect { setup }.to change { ::Group.count }.by(1)
+      end
+
+      context 'when failed to create a group' do
+        let(:args) { { root_group_path: '!!!!!' } }
+
+        it 'raises an error' do
+          expect { setup }.to raise_error(RuntimeError)
+        end
+      end
+    end
+
+    context 'when group already exists' do
+      it 'does not create a new group' do
+        expect { setup }.not_to change { ::Group.count }
+      end
+    end
+
+    it_behaves_like 'checks for dev or test env'
+    it_behaves_like 'errors when GITLAB_SIMULATE_SAAS has unexpected value', true
+    it_behaves_like 'enables all necessary feature flags'
+    it_behaves_like 'errors when there is no license'
+
+    context 'when updating application setting' do
+      it 'changes application settings' do
+        expect { setup }.to change {
+                              Gitlab::CurrentSettings.current_application_settings.check_namespace_plan
+                            }.to(true)
+         .and change {
+                Gitlab::CurrentSettings.current_application_settings
+                                              .allow_local_requests_from_web_hooks_and_services
+              }.to(true)
+      end
     end
   end
 
-  context 'when GITLAB_SIMULATE_SAAS is missing' do
+  context 'when simulating SelfManaged: applying for entire instance, no group argument expected' do
     before do
-      stub_const('ENV', { 'GITLAB_SIMULATE_SAAS' => nil })
+      stub_env('GITLAB_SIMULATE_SAAS', '0')
     end
 
-    it 'raises an error' do
-      expect { setup }.to raise_error(RuntimeError)
-    end
-  end
+    let(:args) { {} }
 
-  context 'when updating application setting' do
-    it 'changes application settings' do
-      expect { setup }.to change { Gitlab::CurrentSettings.current_application_settings.check_namespace_plan }.to(true)
-       .and change {
-              Gitlab::CurrentSettings.current_application_settings
-                                            .allow_local_requests_from_web_hooks_and_services
-            }.to(true)
+    context 'when License does not exist' do
+      it 'raises an error' do
+        License.delete_all
+
+        expect { setup }.to raise_error(RuntimeError)
+      end
     end
+
+    it_behaves_like 'errors when GITLAB_SIMULATE_SAAS has unexpected value', false
+    it_behaves_like 'checks for dev or test env'
+    it_behaves_like 'enables all necessary feature flags'
+    it_behaves_like 'errors when there is no license'
   end
 end
